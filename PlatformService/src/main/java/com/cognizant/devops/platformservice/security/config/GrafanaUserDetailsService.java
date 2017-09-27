87793c2880cfe21cd29c2d078799b568dbdef2d8
vganjare <vishal.r.ganjare@gmail.com>
2017-09-27 16:53:23 -0500

    Updating the User details building mechanism for Spring Security. Grafan native users and LDAP users will get the UserDetails from common code.

--- PlatformService/src/main/java/com/cognizant/devops/platformservice/security/config/GrafanaUserDetailsService.java
+++ PlatformService/src/main/java/com/cognizant/devops/platformservice/security/config/GrafanaUserDetailsService.java
@@ -15,27 +15,14 @@
  ******************************************************************************/
 package com.cognizant.devops.platformservice.security.config;
 
-import java.io.UnsupportedEncodingException;
-import java.util.Base64;
-import java.util.HashSet;
-import java.util.Set;
-
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
-
-import com.cognizant.devops.platformcommons.config.ApplicationConfigProvider;
-import com.cognizant.devops.platformcommons.dal.rest.RestHandler;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
-import com.sun.jersey.api.client.ClientResponse;
 
 @Component
 public class GrafanaUserDetailsService implements UserDetailsService {
@@ -46,26 +33,7 @@ public class GrafanaUserDetailsService implements UserDetailsService {
 	
 	@Override
 	public UserDetails loadUserByUsername(String login) throws UsernameNotFoundException {
-		String authHeader = request.getHeader("Authorization");
-		String decodedAuthHeader;
-		try {
-			decodedAuthHeader = new String(Base64.getDecoder().decode(authHeader.split(" ")[1]), "UTF-8");
-			String[] authTokens = decodedAuthHeader.split(":");
-			JsonObject loginRequestParams = new JsonObject();
-			loginRequestParams.addProperty("user", authTokens[0]);
-			loginRequestParams.addProperty("password", authTokens[1]);
-			String loginApiUrl = ApplicationConfigProvider.getInstance().getGrafana().getGrafanaEndpoint()+"/login";
-			ClientResponse grafanaLoginResponse = RestHandler.doPost(loginApiUrl, loginRequestParams, null);
-			JsonObject responseJson = new JsonParser().parse(grafanaLoginResponse.getEntity(String.class)).getAsJsonObject();
-			Set<GrantedAuthority> authorities = new HashSet<GrantedAuthority>();
-			authorities.add(new SimpleGrantedAuthority("VIEWER"));
-			if("Logged in".equals(responseJson.get("message").getAsString())){
-				return new org.springframework.security.core.userdetails.User(authTokens[0], authTokens[1], authorities);
-			}
-		} catch (UnsupportedEncodingException e) {
-			log.error("Unable to load native user details", e);
-		}
-		return null;
+		return GrafanaUserDetailsUtil.getUserDetails(request);
 	}
 
 }
