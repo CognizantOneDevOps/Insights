ad7024f143a6d19eb04dacd52fcd2cd02c9e3853
vganjare <vishal.r.ganjare@gmail.com>
2017-11-14 14:10:59 -0600

    Adding incremental correlations executions.

--- PlatformEngine/src/main/java/com/cognizant/devops/platformengine/modules/correlation/CustomCorrelations.java
+++ PlatformEngine/src/main/java/com/cognizant/devops/platformengine/modules/correlation/CustomCorrelations.java
@@ -21,55 +21,62 @@ public class CustomCorrelations {
 	
 	private void correlateGitAndJira() {
 		Neo4jDBHandler dbHandler = new Neo4jDBHandler();
-		String gitDataFetchCypher = "MATCH (source:DATA:GIT) where not ((source) -[:GIT_COMMIT_WITH_JIRA_KEY]-> (:JIRA:DATA)) WITH { uuid: source.uuid, commitId: source.commitId, message: source.message} as data limit 500 return collect(data)";
 		try {
-			GraphResponse response = dbHandler.executeCypherQuery(gitDataFetchCypher);
-			JsonObject json = response.getJson();
-			JsonArray rows = json.get("results").getAsJsonArray().get(0).getAsJsonObject().get("data").getAsJsonArray().get(0).getAsJsonObject().get("row").getAsJsonArray();
-			if(rows.isJsonNull() || rows.size() == 0) {
-				return;
-			}
-			JsonArray dataList = rows.get(0).getAsJsonArray();
-			List<String> correlationCyphers = new ArrayList<String>();
-			for(JsonElement dataElem : dataList) {
-				JsonObject dataJson = dataElem.getAsJsonObject();
-				JsonElement messageElem = dataJson.get("message");
-				if(messageElem.isJsonPrimitive()) {
-					String message = messageElem.getAsString();
-					message = message.replaceAll("- ", "-");
-					message = message.replaceAll(" -", "-");
-					String[] tokens = message.split(" ");
-					List<String> jiraKeys = new ArrayList<String>();
-					for(String token : tokens) {
-						if(token.contains("-")) {
-							String[] subTokens = token.split("-");
-							if(subTokens.length > 1) {
-								try {
-									int numPart = Integer.valueOf(subTokens[1]);
-									jiraKeys.add(subTokens[0].trim()+"-"+numPart);
-								}catch(Exception e) {
-									log.warn("Unable to parse the message", e);
+			String paginationCypher = "MATCH (source:DATA:GIT) where not ((source) -[:GIT_COMMIT_WITH_JIRA_KEY]-> (:JIRA:DATA)) return count(source) as count";
+			GraphResponse paginationResponse = dbHandler.executeCypherQuery(paginationCypher);
+			int resultCount = paginationResponse.getJson().get("results").getAsJsonArray().get(0).getAsJsonObject().get("data")
+					.getAsJsonArray().get(0).getAsJsonObject().get("row").getAsJsonArray().get(0).getAsInt();
+			while(resultCount > 0) {
+				resultCount = resultCount - 500;
+				String gitDataFetchCypher = "MATCH (source:DATA:GIT) where not ((source) -[:GIT_COMMIT_WITH_JIRA_KEY]-> (:JIRA:DATA)) WITH { uuid: source.uuid, commitId: source.commitId, message: source.message} as data limit 500 return collect(data)";
+				GraphResponse response = dbHandler.executeCypherQuery(gitDataFetchCypher);
+				JsonArray rows = response.getJson().get("results").getAsJsonArray().get(0).getAsJsonObject().get("data")
+						.getAsJsonArray().get(0).getAsJsonObject().get("row").getAsJsonArray();
+				if(rows.isJsonNull() || rows.size() == 0) {
+					return;
+				}
+				JsonArray dataList = rows.get(0).getAsJsonArray();
+				List<String> correlationCyphers = new ArrayList<String>();
+				for(JsonElement dataElem : dataList) {
+					JsonObject dataJson = dataElem.getAsJsonObject();
+					JsonElement messageElem = dataJson.get("message");
+					if(messageElem.isJsonPrimitive()) {
+						String message = messageElem.getAsString();
+						message = message.replaceAll("- ", "-");
+						message = message.replaceAll(" -", "-");
+						String[] tokens = message.split(" ");
+						List<String> jiraKeys = new ArrayList<String>();
+						for(String token : tokens) {
+							if(token.contains("-")) {
+								String[] subTokens = token.split("-");
+								if(subTokens.length > 1) {
+									try {
+										int numPart = Integer.valueOf(subTokens[1]);
+										jiraKeys.add(subTokens[0].trim()+"-"+numPart);
+									}catch(Exception e) {
+										log.warn("Unable to parse the message", e);
+									}
 								}
 							}
 						}
-					}
-					if(jiraKeys.size() > 0) {
-						JsonArray jiraKeyJson = new JsonArray();
-						for(String key : jiraKeys) {
-							jiraKeyJson.add(key);
+						if(jiraKeys.size() > 0) {
+							JsonArray jiraKeyJson = new JsonArray();
+							for(String key : jiraKeys) {
+								jiraKeyJson.add(key);
+							}
+							//We need to add a time filter as well.
+							StringBuffer correlationCypher = new StringBuffer();
+							correlationCypher.append("MATCH (source:DATA:GIT { uuid:").append(dataJson.get("uuid").toString())
+							.append(", commitId:").append(dataJson.get("commitId").toString()).append("})").append("\n");
+							correlationCypher.append("MATCH (destination:JIRA:DATA) where destination.key IN ").append(jiraKeyJson.toString()).append("\n");
+							correlationCypher.append("CREATE UNIQUE (source) -[r:GIT_COMMIT_WITH_JIRA_KEY]-> (destination)");
+							correlationCyphers.add(correlationCypher.toString());
 						}
-						//We need to add a time filter as well.
-						StringBuffer correlationCypher = new StringBuffer();
-						correlationCypher.append("MATCH (source:DATA:GIT { uuid:").append(dataJson.get("uuid").toString())
-								.append(", commitId:").append(dataJson.get("commitId").toString()).append("})").append("\n");
-						correlationCypher.append("MATCH (destination:JIRA:DATA) where destination.key IN ").append(jiraKeyJson.toString()).append("\n");
-						correlationCypher.append("CREATE UNIQUE (source) -[r:GIT_COMMIT_WITH_JIRA_KEY]-> (destination)");
-						correlationCyphers.add(correlationCypher.toString());
 					}
 				}
-			}
-			if(correlationCyphers.size() > 0) {
-				JsonObject queryResponse = dbHandler.bulkCreateCorrelations(correlationCyphers);
+				if(correlationCyphers.size() > 0) {
+					dbHandler.bulkCreateCorrelations(correlationCyphers);
+				}
 			}
 		} catch (GraphDBException e) {
 			log.error(e);
