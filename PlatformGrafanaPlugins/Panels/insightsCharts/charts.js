8c527b9f89474e52f5723e9e3ae738fb2958a129
Muthukrishnan R <Muthukrishnan.r4@cognizant.com>
2017-11-29 17:55:33 +0530

    Angular material dependency issue fixed

---
+++ PlatformGrafanaPlugins/Panels/insightsCharts/charts.js
@@ -0,0 +1,19 @@
+System.register([], function (exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
+    var InsightsCharts;
+    return {
+        setters: [],
+        execute: function () {
+            InsightsCharts = /** @class */ (function () {
+                function InsightsCharts() {
+                    //super();
+                    //this.google = super.getGooleChart();
+                }
+                return InsightsCharts;
+            }());
+            exports_1("InsightsCharts", InsightsCharts);
+        }
+    };
+});
+//# sourceMappingURL=charts.js.map
