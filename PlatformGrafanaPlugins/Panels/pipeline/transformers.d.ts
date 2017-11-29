64325a9f44fd93008d57e71d7e3ae5f4fe1b1cc1
Muthukrishnan R <Muthukrishnan.r4@cognizant.com>
2017-11-29 17:52:25 +0530

    Angular material dependency issue fixed

---
+++ PlatformGrafanaPlugins/Panels/pipeline/transformers.d.ts
@@ -0,0 +1,7 @@
+/// <reference path="../../../../../public/app/headers/common.d.ts" />
+export declare class Transformers {
+    FieldsMetaDataArray: string[];
+    constructor();
+    getFields(data: any): any;
+    insertValueProperty(data: any, cloneObject: any): any;
+}
