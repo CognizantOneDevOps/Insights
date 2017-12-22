99f68ee1c73bdd49ff93678485d913f9b1bc8530
C, Malarvizhi (Cognizant) <Malarvizhi.C@cognizant.com>
2017-12-22 15:10:17 +0530

    datatagging files

---
+++ PlatformEngine/src/main/java/com/cognizant/devops/platformengine/modules/reader/Constants/DatataggingConstants.java
@@ -0,0 +1,15 @@
+package com.cognizant.devops.platformengine.modules.reader.Constants;
+
+public interface DatataggingConstants {
+	String TOOLNAME = "ToolName";
+	String VALUE = "value";
+	String EXCEL_FILE="data/data.csv";
+	String COLON=":";
+	String PROPERTY="property";
+	String TOOL_NAME="toolName";
+	String LEVEL1="level1";
+	String LEVEL2="level2";
+	String LEVEL3="level3";
+	String LEVEL4="level4";
+
+}
