99aa1fc9b858ab2e56932f434464e316987a11f7
C, Malarvizhi (Cognizant) <Malarvizhi.C@cognizant.com>
2017-12-27 16:47:33 +0530

    datatagging changes

--- PlatformEngine/src/main/java/com/cognizant/devops/platformengine/modules/reader/util/DataProcessorUtil.java
+++ PlatformEngine/src/main/java/com/cognizant/devops/platformengine/modules/reader/util/DataProcessorUtil.java
@@ -1,10 +1,14 @@
 package com.cognizant.devops.platformengine.modules.reader.util;
 
 import java.io.IOException;
+import java.sql.SQLIntegrityConstraintViolationException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.log4j.Logger;
 
 import com.cognizant.devops.platformdal.hierarchy.details.HierarchyDetails;
 import com.cognizant.devops.platformdal.hierarchy.details.HierarchyDetailsDAL;
@@ -16,7 +20,6 @@ import com.fasterxml.jackson.dataformat.csv.CsvSchema;
 
 
 public class DataProcessorUtil {
-
 	private static final DataProcessorUtil dataProcessorUtil = new DataProcessorUtil();
 
 	private DataProcessorUtil() {
@@ -29,48 +32,62 @@ public class DataProcessorUtil {
 
 	public  Map<String,List<String>> readData() throws  IOException {
 		Map<String,List<String>> map=new HashMap<String,List<String>>();
-		CsvMapper mapper = new CsvMapper();
-		CsvSchema schema = CsvSchema.emptySchema().withHeader(); 
-		MappingIterator<Map<String,String>> it = mapper.readerFor(Map.class)
-				.with(schema)
-				.readValues(DataProcessorUtil.class.getClassLoader().getResource(DatataggingConstants.EXCEL_FILE));
-		List<HierarchyDetails> hiearchyList=new ArrayList<HierarchyDetails>();
 		HierarchyDetailsDAL hierarchyDetailsDAL = new HierarchyDetailsDAL();
-		while (it.hasNext()) {
-			HierarchyDetails hierarchyDetails = new HierarchyDetails();
-			Map<String,String> rowAsMap = it.next();
-			for (Map.Entry<String, String> entry : rowAsMap.entrySet()) {
-				String key = entry.getKey();
-				if(key.equals(DatataggingConstants.TOOLNAME)){
-
-
-				}else if(key.equals(DatataggingConstants.LEVEL1)){
-					hierarchyDetails.setLevel_1(entry.getValue());
-				}else if(key.equals(DatataggingConstants.LEVEL2)){
-					hierarchyDetails.setLevel_1(entry.getValue());
-				}else if(key.equals(DatataggingConstants.LEVEL3)){
-					hierarchyDetails.setLevel_1(entry.getValue());
-				}else if(key.equals(DatataggingConstants.LEVEL4)){
-					hierarchyDetails.setLevel_1(entry.getValue());
-				}else if(key.equals(DatataggingConstants.PROPERTY)){
-					hierarchyDetails.setProperty(entry.getValue());
-				}else if(key.equals(DatataggingConstants.VALUE)){
-					hierarchyDetails.setValue(entry.getValue());
-				}
+		CsvSchema  schemaDet =CsvSchema.builder()
+								.addColumn(DatataggingConstants.ID,CsvSchema.ColumnType.NUMBER)
+								.addColumn(DatataggingConstants.LEVEL1,CsvSchema.ColumnType.STRING)
+								.addColumn(DatataggingConstants.LEVEL2,CsvSchema.ColumnType.STRING)
+								.addColumn(DatataggingConstants.LEVEL3,CsvSchema.ColumnType.STRING)
+								.addColumn(DatataggingConstants.LEVEL4,CsvSchema.ColumnType.STRING)
+								.addColumn(DatataggingConstants.TOOL_NAME,CsvSchema.ColumnType.STRING)
+								.addColumn(DatataggingConstants.TOOL_PROPERTY,CsvSchema.ColumnType.STRING)
+								.addColumn(DatataggingConstants.PROPERTY_VALUE,CsvSchema.ColumnType.STRING)
+								.build();
+		CsvMapper mapperDet = new CsvMapper();
+		MappingIterator<HierarchyDetails> iterator = mapperDet.readerFor(HierarchyDetails.class).with(schemaDet.withHeader())
+				  .readValues(DataProcessorUtil.class.getClassLoader().getResource(DatataggingConstants.EXCEL_FILE));
+		List<HierarchyDetails> allDetails = iterator.readAll();
+		List<HierarchyDetails> details = new ArrayList<HierarchyDetails>();
+		for(HierarchyDetails  det:allDetails){
+			StringBuilder hiearchyData= new StringBuilder();
+			String appenddata="";
+			if(!StringUtils.isBlank(det.getLevel_1())){
+				hiearchyData.append(appenddata);
+				hiearchyData.append(det.getLevel_1());
+				hiearchyData.append(DatataggingConstants.COLON);
+				
 			}
-			hiearchyList.add(hierarchyDetails);
+			if(!StringUtils.isBlank(det.getLevel_2())){
+				hiearchyData.append(appenddata);
+				hiearchyData.append(det.getLevel_2());
+				hiearchyData.append(DatataggingConstants.COLON);
+				
+			}
+			if(!StringUtils.isBlank(det.getLevel_3())){
+				hiearchyData.append(appenddata);
+				hiearchyData.append(det.getLevel_3());
+				hiearchyData.append(DatataggingConstants.COLON);
+				
+			}
+			if(!StringUtils.isBlank(det.getLevel_4())){
+				hiearchyData.append(appenddata);
+				hiearchyData.append(det.getLevel_1());
+				hiearchyData.append(DatataggingConstants.COLON);
+				
+			}
+			det.setHierarchyName(StringUtils.removeEnd(hiearchyData.toString() ,DatataggingConstants.COLON));
+			details.add(det);
 		}
-
-		hierarchyDetailsDAL.addHierarchyDetailsList(hiearchyList);
-		return map;
+					hierarchyDetailsDAL.addHierarchyDetailsList(details);
+				return map;
 	}
 
 
-	/*public static void main(String[] a) throws IOException{
+/*	public static void main(String[] a) throws IOException{
 		DataProcessorUtil processor=new DataProcessorUtil();
 		processor.readData();
 
-	}*/
-
+	}
+*/
 
 }
