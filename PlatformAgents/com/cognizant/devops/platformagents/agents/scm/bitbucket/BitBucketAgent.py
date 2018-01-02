ab932e71e692dbc8fd1fb54dac1fc0552d8b93e3
Vaibhav Joshi <vaibhav.joshi@kp.org>
2018-01-01 16:10:29 -0800

    Added support to get the pull request data in neo4j

--- PlatformAgents/com/cognizant/devops/platformagents/agents/scm/bitbucket/BitBucketAgent.py
+++ PlatformAgents/com/cognizant/devops/platformagents/agents/scm/bitbucket/BitBucketAgent.py
@@ -23,13 +23,13 @@ from time import mktime
 from dateutil import parser
 from com.cognizant.devops.platformagents.core.BaseAgent import BaseAgent
 
-
 class BitBucketAgent(BaseAgent):
     def process(self):
         self.baseEndPoint = self.config.get("BaseEndPoint", '')
         self.userId = self.config.get("UserID", '')
         self.passwd = self.config.get("Passwd", '')
         self.scanAllBranches = self.config.get("scanAllBranches", False)
+        self.scanPullRequests = self.config.get("scanPullRequests", False)
         startFrom = self.config.get("StartFrom", '')
         startFrom = parser.parse(startFrom)
         startFrom = mktime(startFrom.timetuple()) + startFrom.microsecond/1000000.0
@@ -93,6 +93,9 @@ class BitBucketAgent(BaseAgent):
                             branchName = "master"
                             self.processAllCommitsForBranch(projKey, repoName, branchName, repoTracking)
 
+                        if self.scanPullRequests : # process pull requests for this repo
+                            self.processPullRequestsForRepo(projKey, repoName, repoTracking)
+
                     if bitBicketRepos.get("isLastPage", True):
                         fetchNextRepoPage = False
                         break;
@@ -144,6 +147,59 @@ class BitBucketAgent(BaseAgent):
             self.publishToolsData(data)
             self.updateTrackingJson(self.tracking)
 
+
+    def processPullRequestsForRepo(self,projKey,repoName, repoTracking):
+        pullRequestTemplate = self.responseTemplate.get("pullRequests",None)
+
+        data = []
+        injectData = {}
+        injectData['type'] = "pullRequest"
+        injectData['repoName'] = repoName
+        injectData['projectName'] = projKey
+        # get all commits under a branch
+        limit = 100
+        prStart = 0
+        fetchNextPRPage = True
+        lastUpdatedPRIdTracking = repoTracking.get("lastUpdatedPullRequestId", 0)
+        pullRequests = {} # initialize, in case of any exception
+        # Logic : Fetch pull requests in the order to newest first. In the first fetch we would need to get all pull requests for the repoTracking.
+        # subsequent runs fetch pull reuqests on till oldest open PR. This would help reduece not to get merged or declined pr which are alread indexed
+        # lastOpenPullRequestId should be updated to oldest
+        lastUpdatedPRId = None
+        while fetchNextPRPage:
+            pullRequestUrl = self.baseEndPoint+projKey+"/repos/"+repoName+"/pull-requests?state=All&order=NEWEST&limit="+str(limit)+'&start='+str(prStart)
+            try:
+                pullRequests = self.getResponse(pullRequestUrl, 'GET', self.userId, self.passwd, None)
+                i = 0
+                for pullReq in (pullRequests["values"]):
+                    createdDate = pullRequests["values"][i]["createdDate"]
+                    isOpenPR = pullRequests["values"][i]["open"]
+                    prId = pullRequests["values"][i]["id"]
+                    if lastUpdatedPRId == None :
+                        lastUpdatedPRId = prId # assigned the first PR to this variable, irrespective of status,
+
+                    if  prId < lastUpdatedPRIdTracking or createdDate < self.startFrom : # no need to process remaining pull requests
+                        fetchNextPRPage = False
+                        break;
+
+                    if  self.startFrom <= createdDate : # publish these PR data, if its within the date range
+                        data += self.parseResponse(pullRequestTemplate, pullReq, injectData)
+                        if isOpenPR: # if the PR is Open, we would need to get PR till this PRId in next run
+                            lastUpdatedPRId = prId
+                    i = i + 1
+            except:
+                pass
+            if pullRequests.get("isLastPage", True):
+                fetchNextPRPage = False
+                break;
+            prStart = pullRequests.get("nextPageStart", None)
+
+        # Update data once its processed for each repositor
+        if len(data) > 0:
+            self.publishToolsData(data)
+        if lastUpdatedPRId != None :
+            repoTracking["lastUpdatedPullRequestId"] = lastUpdatedPRId
+            self.updateTrackingJson(self.tracking)
 
 if __name__ == "__main__":
     BitBucketAgent()
