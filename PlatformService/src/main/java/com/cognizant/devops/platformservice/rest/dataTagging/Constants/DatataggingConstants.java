2f8e5f86fbba5946faadc9d9c62bf2e67690680d
C, Malarvizhi (Cognizant) <Malarvizhi.C@cognizant.com>
2018-01-04 14:42:27 +0530

    csv reader

---
+++ PlatformService/src/main/java/com/cognizant/devops/platformservice/rest/dataTagging/Constants/DatataggingConstants.java
@@ -0,0 +1,18 @@
+package com.cognizant.devops.platformservice.rest.dataTagging.Constants;
+
+public interface DatataggingConstants {
+	String TOOLNAME = "ToolName";
+	String VALUE = "value";
+	String EXCEL_FILE="data/data.csv";
+	String COLON=":";
+//	String PROPERTY="property";
+	String LEVEL1="level_1";
+	String LEVEL2="level_2";
+	String LEVEL3="level_3";
+	String LEVEL4="level_4";
+	String ID="id";
+	String TOOL_NAME="toolName";
+	String TOOL_PROPERTY="toolProperty";
+	String PROPERTY_VALUE="propertyValue";
+
+}
