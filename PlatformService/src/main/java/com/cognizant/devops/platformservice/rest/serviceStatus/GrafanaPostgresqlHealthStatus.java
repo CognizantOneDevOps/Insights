897234ef0272d0e6c1b3abae288bb3bbba21aee0
Akshay <Akshay.Sharma4@cognizant.com>
2018-01-22 15:36:17 +0530

    Service to check PostgreSQL DB health status

--- PlatformService/src/main/java/com/cognizant/devops/platformservice/rest/serviceStatus/GrafanaPostgresqlHealthStatus.java
+++ PlatformService/src/main/java/com/cognizant/devops/platformservice/rest/serviceStatus/GrafanaPostgresqlHealthStatus.java
@@ -13,7 +13,7 @@
  * License for the specific language governing permissions and limitations under
  * the License.
  ******************************************************************************/
-package com.cognizant.devops.platformservice.rest.grafana;
+package com.cognizant.devops.platformservice.rest.serviceStatus;
 
 import org.apache.log4j.Logger;
 import org.springframework.http.MediaType;
