e17815b18397d56b88acba94a743db71a090046a
Vishal Ganjare <vishal.r.ganjare@gmail.com>
2018-02-01 14:38:41 -0600

    Engine: Adding configurations to control the correlation mechanism.

---
+++ PlatformCommons/src/main/java/com/cognizant/devops/platformcommons/config/CorrelationData.java
@@ -0,0 +1,26 @@
+package com.cognizant.devops.platformcommons.config;
+
+public class CorrelationData {
+	private int correlationWindow = 48;
+	private int correlationFrequency = 4;
+	private int batchSize = 2000;
+	
+	public int getCorrelationWindow() {
+		return correlationWindow;
+	}
+	public void setCorrelationWindow(int correlationWindow) {
+		this.correlationWindow = correlationWindow;
+	}
+	public int getCorrelationFrequency() {
+		return correlationFrequency;
+	}
+	public void setCorrelationFrequency(int correlationFrequency) {
+		this.correlationFrequency = correlationFrequency;
+	}
+	public int getBatchSize() {
+		return batchSize;
+	}
+	public void setBatchSize(int batchSize) {
+		this.batchSize = batchSize;
+	}
+}
