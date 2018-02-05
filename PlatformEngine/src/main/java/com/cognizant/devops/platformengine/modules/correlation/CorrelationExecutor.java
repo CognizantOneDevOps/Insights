df45f38837372080b866bc67d5077c7a2af6358e
Vishal Ganjare <vishal.r.ganjare@gmail.com>
2018-02-05 14:29:01 -0600

    Adding copyright statement.

--- PlatformEngine/src/main/java/com/cognizant/devops/platformengine/modules/correlation/CorrelationExecutor.java
+++ PlatformEngine/src/main/java/com/cognizant/devops/platformengine/modules/correlation/CorrelationExecutor.java
@@ -1,3 +1,18 @@
+/*******************************************************************************
+ * Copyright 2017 Cognizant Technology Solutions
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License.  You may obtain a copy
+ * of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ ******************************************************************************/
 package com.cognizant.devops.platformengine.modules.correlation;
 
 import java.io.BufferedReader;
@@ -96,7 +111,6 @@ public class CorrelationExecutor {
 						.get("data").getAsJsonArray().get(0).getAsJsonObject()
 						.get("row").getAsInt();
 				log.debug("Pre Processed "+destinationToolName+" records: "+processedRecords+" in: "+(System.currentTimeMillis() - st) + " ms");
-				System.out.println("Pre Processed "+destinationToolName+" records: "+processedRecords+" in: "+(System.currentTimeMillis() - st) + " ms");
 			}
 		} catch (GraphDBException e) {
 			log.error("Error occured while loading the destination data for correlations.", e);
@@ -192,7 +206,6 @@ public class CorrelationExecutor {
 					.get("data").getAsJsonArray().get(0).getAsJsonObject()
 					.get("row").getAsInt();
 			log.debug("Correlated "+destination.getToolName()+" records: "+processedRecords+" in: "+(System.currentTimeMillis() - st) + " ms");
-			System.out.println("Correlated "+destination.getToolName()+" records: "+processedRecords+" in: "+(System.currentTimeMillis() - st) + " ms");
 		} catch (GraphDBException e) {
 			log.error("Error occured while executing correlations for relation "+relationName+".", e);
 		}
@@ -270,10 +283,4 @@ public class CorrelationExecutor {
 		maxCorrelationTime = currentCorrelationTime + correlations.getCorrelationWindow() * 60 * 60;
 		lastCorrelationTime = currentCorrelationTime - correlations.getCorrelationFrequency() * 60 * 60;
 	}
-	
-	/*public static void main(String[] args) {
-		ApplicationConfigCache.loadConfigCache();
-		CorrelationExecutor cor = new CorrelationExecutor();
-		cor.execute();
-	}*/
 }
