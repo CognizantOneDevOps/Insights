95ebb1ee419a533285ab336bb3f5ab2d7258b4af
C, Malarvizhi (Cognizant) <Malarvizhi.C@cognizant.com>
2018-02-13 17:15:14 +0530

    version changes

--- PlatformService/src/main/java/com/cognizant/devops/platformservice/agentmanagement/service/AgentManagementService.java
+++ PlatformService/src/main/java/com/cognizant/devops/platformservice/agentmanagement/service/AgentManagementService.java
@@ -16,10 +16,16 @@
 
 package com.cognizant.devops.platformservice.agentmanagement.service;
 
+import org.springframework.stereotype.Component;
+
+import com.google.gson.JsonObject;
+
+
 public interface AgentManagementService {
 
 	public String registerAgent(String configDetails);
 	public String installAgent(String agentId,String toolName);
 	public String startStopAgent(String agentId,String action);
 	public String updateAgent(String agentId, String configDetails);
+	public JsonObject getAgentDetails();
 }
