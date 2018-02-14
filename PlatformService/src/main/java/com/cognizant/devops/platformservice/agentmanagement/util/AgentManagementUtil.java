2a324ab517ffcc226ce7f6a019cdd3358d2d765c
C, Malarvizhi (Cognizant) <Malarvizhi.C@cognizant.com>
2018-02-14 18:02:47 +0530

    agent changes

---
+++ PlatformService/src/main/java/com/cognizant/devops/platformservice/agentmanagement/util/AgentManagementUtil.java
@@ -0,0 +1,119 @@
+/*******************************************************************************
+ *  * Copyright 2017 Cognizant Technology Solutions
+ *  * 
+ *  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ *  * use this file except in compliance with the License.  You may obtain a copy
+ *  * of the License at
+ *  * 
+ *  *   http://www.apache.org/licenses/LICENSE-2.0
+ *  * 
+ *  * Unless required by applicable law or agreed to in writing, software
+ *  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ *  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ *  * License for the specific language governing permissions and limitations under
+ *  * the License.
+ *******************************************************************************/
+package com.cognizant.devops.platformservice.agentmanagement.util;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
+import java.util.Enumeration;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+public class AgentManagementUtil {
+
+	private static final AgentManagementUtil agentManagementUtil = new AgentManagementUtil();
+
+	private AgentManagementUtil() {
+
+	}
+
+	public static AgentManagementUtil getInstance() {
+		return agentManagementUtil;
+	}
+
+	public  JsonObject unZipArchive(URL url, File targetDir) throws IOException {
+		/*System.setProperty("http.proxyHost", "proxy.cognizant.com");
+		System.setProperty("http.proxyPort","6050");*/
+		if (!targetDir.exists()) {
+			targetDir.mkdirs();
+		}
+		InputStream in = new BufferedInputStream(url.openStream(), 1024);
+		File zip = File.createTempFile("agent_", ".zip", targetDir);
+		OutputStream out = new BufferedOutputStream(new FileOutputStream(zip));
+		copyInputStream(in, out);
+		out.close();
+		return unpackArchive(zip, targetDir);
+	}
+
+	public  JsonObject unpackArchive(File zip, File targetDir) throws IOException {
+		if (!zip.exists()) {
+			throw new IOException(zip.getAbsolutePath() + " does not exist");
+		}
+		if (!buildDirectory(targetDir)) {
+			throw new IOException("Could not create directory: " + targetDir);
+		}
+		ZipFile zipFile = new ZipFile(zip);
+		String filePath=null;
+		for (Enumeration entries = zipFile.entries(); entries.hasMoreElements();) {
+			ZipEntry entry = (ZipEntry) entries.nextElement();
+			File file = new File(targetDir, File.separator + entry.getName());
+
+			if (!buildDirectory(file.getParentFile())) {
+				throw new IOException("Could not create directory: " + file.getParentFile());
+			}
+
+			if (!entry.isDirectory()) {
+				copyInputStream(zipFile.getInputStream(entry), new BufferedOutputStream(new FileOutputStream(file)));
+				if(entry.getName().endsWith("config.json")){
+					filePath=entry.getName();
+				}
+
+			} else {
+				if (!buildDirectory(file)) {
+					throw new IOException("Could not create directory: " + file);
+				}
+			}
+		}
+		zipFile.close();
+		JsonObject jsonObject = readJsonFile(targetDir, filePath);
+		return jsonObject;
+	}
+
+	private  JsonObject readJsonFile(File targetDir, String filePath) throws FileNotFoundException {
+		JsonParser  parser = new JsonParser();
+		Object obj = parser.parse(new FileReader(targetDir+"/"+filePath));
+		JsonObject jsonObject = (JsonObject)obj;
+		return jsonObject;
+	}
+
+	public  void copyInputStream(InputStream in, OutputStream out) throws IOException {
+		byte[] buffer = new byte[1024];
+		int len = in.read(buffer);
+		while (len >= 0) {
+			out.write(buffer, 0, len);
+			len = in.read(buffer);
+		}
+		in.close();
+		out.close();
+	}
+
+	public  boolean buildDirectory(File file) {
+		return file.exists() || file.mkdirs();
+	}
+
+
+
+}
