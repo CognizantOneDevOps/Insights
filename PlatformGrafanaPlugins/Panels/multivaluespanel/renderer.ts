be6c0866706b57e1f0b7702996c64ba0b3d16896
CTS\463188 <mayank.gangwal@cognizant.com>
2018-02-23 20:44:51 +0530

    Removing unwanted code

--- PlatformGrafanaPlugins/Panels/multivaluespanel/renderer.ts
+++
@@ -1,8 +0,0 @@
-///<reference path="../../../headers/common.d.ts" />
-
-import _ from 'lodash';
-import moment from 'moment';
-import kbn from 'app/core/utils/kbn';
-
-export class TableRenderer {}
-
