80d33fd2bf0dd386886c61a082c7e37346956798
Vishwajit Mankar <vishwajit.mankar@cognizant.com>
2018-03-15 19:34:04 +0530

    Fixed unit testing outcomes.

--- PlatformAgents/com/cognizant/devops/platformagents/agents/scm/git/installagent.sh
+++ PlatformAgents/com/cognizant/devops/platformagents/agents/scm/git/installagent.sh
@@ -3,22 +3,27 @@ opt=$1
 echo "$opt"
 case $opt in
         [lL][Ii][nN][uU][Xx])
-                echo "Running on Linux..."
+                echo "Git Running on Linux..."
 				sudo cp -xp InSightsGitAgent.sh  /etc/init.d/InSightsGitAgent
 				sudo chmod +x /etc/init.d/InSightsGitAgent
 				sudo chkconfig InSightsGitAgent on
 				sudo service  InSightsGitAgent status
-				echo "Service installed and started"
+				sudo service  InSightsGitAgent stop
+				sudo service  InSightsGitAgent status
+				sudo service  InSightsGitAgent start
+				sudo service  InSightsGitAgent status
+				
+				echo "Service installaton steps completed"
                 ;;
         [uU][bB][uU][nN][tT][uU])
-                echo "Running on Ubuntu..."
+                echo "Git Running on Ubuntu..."
 				sudo cp -xp InSightsGitAgent.service /etc/systemd/system
 				sudo systemctl enable InSightsGitAgent
 				sudo systemctl start InSightsGitAgent
-				echo "Service Installed and started"
+				echo "Service installaton steps completed"
                 ;;
         centos)
-                echo "Running on centso..."
+                echo "Git Running on centso..."
                 ;;
         *)
         	    echo "Please provide correct OS input"
