c539baac4c2ce48b27581169756e13faa4ebd71a
sinswa28 <swati.banerjee@cognizant.com>
2018-03-16 13:27:40 +0000

    key names in config.json files and retrieval code has been modified

--- PlatformAgents/com/cognizant/devops/platformagents/agents/scm/bitbucket/BitBucketAgentAllBranches.py
+++ PlatformAgents/com/cognizant/devops/platformagents/agents/scm/bitbucket/BitBucketAgentAllBranches.py
@@ -24,10 +24,10 @@ from com.cognizant.devops.platformagents.core.BaseAgent import BaseAgent
 
 class BitBucketAgentAllBranches(BaseAgent):
     def process(self):
-        BaseEndPoint = self.config.get("BaseEndPoint", '')
-        UserId = self.config.get("UserID", '')
-        Passwd = self.config.get("Passwd", '')
-        startFrom = self.config.get("StartFrom", '')
+        BaseEndPoint = self.config.get("baseEndPoint", '')
+        UserId = self.config.get("userID", '')
+        Passwd = self.config.get("passwd", '')
+        startFrom = self.config.get("startFrom", '')
         startFrom = parser.parse(startFrom)
         startFrom = mktime(startFrom.timetuple()) + startFrom.microsecond/1000000.0
         startFrom = long(startFrom * 1000)
