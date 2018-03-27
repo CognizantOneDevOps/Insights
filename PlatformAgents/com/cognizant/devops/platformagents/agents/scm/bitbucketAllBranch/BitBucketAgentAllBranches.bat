de8811959516854b006918dc7f2b90c7e8b10dd8
C, Malarvizhi (Cognizant) <Malarvizhi.C@cognizant.com>
2018-03-27 17:37:17 +0530

    bitbucket agent file clean up

---
+++ PlatformAgents/com/cognizant/devops/platformagents/agents/scm/bitbucketAllBranch/BitBucketAgentAllBranches.bat
@@ -0,0 +1,2 @@
+pushd %INSIGHTS_AGENT_HOME%\PlatformAgents\bitbucket
+python -c "from com.cognizant.devops.platformagents.agents.scm.bitbucketAllBranch.BitBucketAgentAllBranches  import BitBucketAgentAllBranches; BitBucketAgentAllBranches()"
