0852eab446c9ac94a0a12969cdadc8a3bc1f276b
qa <qabox@cognizant.com>
2018-03-29 13:07:07 +0000

    refractored code

---
+++ PlatformService/src/main/java/com/cognizant/devops/platformservice/rest/datatagging/constants/DatataggingConstants.java
@@ -0,0 +1,26 @@
+package com.cognizant.devops.platformservice.rest.datatagging.constants;
+
+public final class DatataggingConstants {
+
+	public static final String TOOLNAME = "ToolName";
+	public static final String VALUE = "value";
+	public static final String EXCEL_FILE="data/data.csv";
+	public static final String COLON=":";
+	public static final String LEVEL1="level_1";
+	public static final String LEVEL2="level_2";
+	public static final String LEVEL3="level_3";
+	public static final String LEVEL4="level_4";
+	public static final String ID="id";
+	public static final String TOOL_NAME = "toolName";
+	public static final String TOOL_PROPERTY = "toolProperty";
+	public static final String PROPERTY_VALUE = "propertyValue";
+	public static final String HIEARCHYNAME = "hiearchyName";
+	public static final String CREATIONDATE = "creationDate";
+	public static final String NAME = "name";
+	public static final String CHILDREN = "children";
+	public static final String UNDEFINED = "undefined";
+	public static final String RESPONSE = "response";
+	public static final String ERRORS = "errors";
+	public static final String ACTION = "Action";
+
+}
