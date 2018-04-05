2236887b6a78c34e415fae3901c5e94bd8c04b08
C, Malarvizhi (Cognizant) <Malarvizhi.C@cognizant.com>
2018-04-05 16:03:47 +0530

    intial version changes

--- PlatformService/src/main/java/com/cognizant/devops/platformservice/rest/datatagging/EntityDefinitionService.java
+++ PlatformService/src/main/java/com/cognizant/devops/platformservice/rest/datatagging/EntityDefinitionService.java
@@ -13,7 +13,7 @@
  * License for the specific language governing permissions and limitations under
  * the License.
  ******************************************************************************/
-package com.cognizant.devops.platformservice.rest.datatagging;
+package com.cognizant.devops.platformservice.rest.dataTagging;
 
 import java.util.List;
 
--- PlatformService/src/main/java/com/cognizant/devops/platformservice/rest/datatagging/config/DataTaggingConfig.java
+++ PlatformService/src/main/java/com/cognizant/devops/platformservice/rest/datatagging/config/DataTaggingConfig.java
@@ -13,7 +13,7 @@
  * License for the specific language governing permissions and limitations under
  * the License.
  ******************************************************************************/
-package com.cognizant.devops.platformservice.rest.datatagging.config;
+package com.cognizant.devops.platformservice.rest.dataTagging.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
