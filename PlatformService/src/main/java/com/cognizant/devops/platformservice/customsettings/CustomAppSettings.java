d258e26eb84425f92692526e4455e0a7a411b64f
sinswa28 <swati.banerjee@cognizant.com>
2018-04-06 13:13:50 +0000

    Code changes for DataPurging Engine job

--- PlatformService/src/main/java/com/cognizant/devops/platformservice/customsettings/CustomAppSettings.java
+++ PlatformService/src/main/java/com/cognizant/devops/platformservice/customsettings/CustomAppSettings.java
@@ -13,7 +13,7 @@
 	 * License for the specific language governing permissions and limitations under
 	 * the License.
 	 ******************************************************************************/
-package com.cognizant.devops.platformservice.rest.customsettings;
+package com.cognizant.devops.platformservice.customsettings;
 
 import java.io.File;
 import java.io.FilenameFilter;
