0666d4e8cd236160717f23f203d1cb1fc76a1dae
SowmiyaRanganathan <31270309+SowmiyaRanganathan@users.noreply.github.com>
2018-04-10 16:02:56 +0530

    Update insights_python.sh

--- Deployment/RHEL7/reference-DocRoot-Scripts/insights_python.sh
+++ Deployment/RHEL7/reference-DocRoot-Scripts/insights_python.sh
@@ -15,6 +15,6 @@ source /etc/profile
 sleep 5
 echo "Get all avaiable Python Agents"
 cd $INSIGHTS_AGENT_HOME
-sudo wget http://platform.cogdevops.com/insights_install/installationScripts/latest/RHEL/agents/PlatformAgents.zip -O PlatformAgents.zip
+sudo wget http://platform.cogdevops.com/insights_install/release/latest/PlatformAgents.zip -O PlatformAgents.zip
 sudo unzip PlatformAgents.zip && sudo rm -rf PlatformAgents.zip
 
