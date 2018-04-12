46d43d80a1f7ea72af7f1f5005c2c9c547b45e90
Vishwajit Mankar <vishwajit.mankar@cognizant.com>
2018-04-12 12:54:15 +0530

    Fixed merge issue

---
+++ PlatformService/src/main/java/com/cognizant/devops/platformservice/rest/datatagging/util/DataProcessorUtil.java
@@ -0,0 +1,193 @@
+/*******************************************************************************
+ * Copyright 2017 Cognizant Technology Solutions
+ *   
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License.  You may obtain a copy
+ * 	of the License at
+ *   
+ * 	http://www.apache.org/licenses/LICENSE-2.0
+ *   
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ ******************************************************************************/
+package com.cognizant.devops.platformservice.rest.datatagging.util;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+import org.apache.log4j.Logger;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.cognizant.devops.platformcommons.dal.neo4j.GraphDBException;
+import com.cognizant.devops.platformcommons.dal.neo4j.Neo4jDBHandler;
+import com.cognizant.devops.platformservice.rest.datatagging.constants.DatataggingConstants;
+import com.google.gson.JsonObject;
+
+
+public class DataProcessorUtil  {
+	private static final DataProcessorUtil dataProcessorUtil = new DataProcessorUtil();
+	private static final Logger log = Logger.getLogger(DataProcessorUtil.class);
+	private DataProcessorUtil() {
+
+	}
+
+	public static DataProcessorUtil getInstance() {
+		return dataProcessorUtil;
+	}
+
+	public  boolean createBusinessHierarchyMetaData(MultipartFile file)    {
+
+		File csvfile =null;
+		boolean status = false;
+		try {
+			csvfile = convertToFile(file);
+		} catch (IOException ex) {
+			log.debug("Exception while creating csv on server",ex);
+			return status;
+		}
+		CSVFormat format = CSVFormat.newFormat(',').withHeader();
+		try (Reader reader = new FileReader(csvfile); CSVParser csvParser = new CSVParser(reader, format);){
+
+			Neo4jDBHandler dbHandler = new Neo4jDBHandler();
+			Map<String, Integer> headerMap = csvParser.getHeaderMap();
+			dbHandler.executeCypherQuery("CREATE CONSTRAINT ON (n:METADATA) ASSERT n.metadata_id  IS UNIQUE");
+			String query =  "UNWIND {props} AS properties " +
+					"CREATE (n:METADATA:DATATAGGING) " +
+					"SET n = properties";
+			status = parseCsvRecords(status, csvParser, dbHandler, headerMap, query);
+
+		} catch (FileNotFoundException e) {
+			log.error("File not found Exception in uploading csv file" , e);
+		} catch (IOException  | GraphDBException e ) {
+			log.error("IOException in uploading csv file" , e);
+		}
+		return status;
+
+	}
+
+	private boolean parseCsvRecords(boolean status, CSVParser csvParser, Neo4jDBHandler dbHandler,
+			Map<String, Integer> headerMap, String query) throws IOException, GraphDBException {
+		List<JsonObject> nodeProperties = new ArrayList<>();
+		for (CSVRecord csvRecord : csvParser.getRecords()) {
+			JsonObject json = getHierachyDetails(csvRecord, headerMap);			
+			json.addProperty(DatataggingConstants.CREATIONDATE, Instant.now().toEpochMilli() );
+			nodeProperties.add(json);
+		}
+		JsonObject graphResponse = dbHandler.bulkCreateNodes(nodeProperties, null, query);
+		if(graphResponse.get(DatataggingConstants.RESPONSE).getAsJsonObject().get(DatataggingConstants.ERRORS).getAsJsonArray().size() > 0){
+			log.error(graphResponse);
+			return status;
+		}
+		
+		return true;
+	}
+
+	public boolean updateHiearchyProperty(MultipartFile file) {
+		Neo4jDBHandler dbHandler = new Neo4jDBHandler();
+		File csvfile =null;
+		boolean status = false;
+		try {
+			csvfile = convertToFile(file);
+		} catch (IOException e) {
+			log.debug("Exception while creating csv on server",e);
+			return status;
+		}
+		String label = "METADATA:DATATAGGING";
+		CSVFormat format = CSVFormat.newFormat(',').withHeader();
+
+		try (Reader reader = new FileReader(csvfile); CSVParser csvParser = new CSVParser(reader, format);){
+			Map<String, Integer> headerMap = csvParser.getHeaderMap();
+			List<JsonObject>  editList = new ArrayList<>();
+			List<JsonObject>  deleteList = new ArrayList<>();
+			for (CSVRecord record : csvParser) { 
+
+				if( record.get(DatataggingConstants.ACTION) != null &&  record.get(DatataggingConstants.ACTION).equals("edit")){
+					JsonObject json = getHierachyDetails(record, headerMap);
+					editList.add(json);
+				}else if(record.get(DatataggingConstants.ACTION) != null &&  record.get(DatataggingConstants.ACTION).equals("delete") ){
+					JsonObject json = getHierachyDetails(record, headerMap);
+					deleteList.add(json);
+				}
+
+			}
+			if(!editList.isEmpty() ){
+				status = updateMedataNodes(dbHandler, status, label, editList);
+			}	
+			if( !deleteList.isEmpty()){
+				status = deleteMedataNodes(dbHandler, status, label, deleteList);
+			}
+
+		} catch (IOException e) {
+			log.error("Exception in updating metadata" , e);
+		} 
+		return status;
+	}
+
+	private boolean deleteMedataNodes(Neo4jDBHandler dbHandler, boolean status, String label,
+			List<JsonObject> deleteList) {
+		String cypherQuery;
+		cypherQuery = " UNWIND {props} AS properties MATCH (n :"+label+"{metadata_id:properties.metadata_id})   "
+				+ " REMOVE n:"+label+"  SET n:METADATA_BACKUP  RETURN n ";
+		try {
+			JsonObject graphResponse = dbHandler.executeQueryWithData(cypherQuery,deleteList);
+			if(graphResponse.get(DatataggingConstants.RESPONSE).getAsJsonObject().get(DatataggingConstants.ERRORS).getAsJsonArray().size() > 0){
+				return status;
+			}
+			status = true;
+		} catch (GraphDBException e) {
+			log.error("Exception in deleting nodes ",e);
+		}
+		return status;
+	}
+
+	private boolean updateMedataNodes(Neo4jDBHandler dbHandler, boolean status, String label, List<JsonObject> editList) {
+		String cypherQuery;
+		cypherQuery = " UNWIND {props} AS properties MATCH (n :"+label+"{metadata_id:properties.metadata_id}) "
+				+ " SET n += {props} RETURN n ";
+		try {
+			JsonObject graphResponse = dbHandler.executeQueryWithData(cypherQuery,editList);
+			if(graphResponse.get(DatataggingConstants.RESPONSE).getAsJsonObject().get(DatataggingConstants.ERRORS).getAsJsonArray().size() > 0){
+				return status;
+			}
+			status = true;
+		} catch (GraphDBException e) {
+			log.error(e);
+		}
+		return status;
+	}
+	
+	private JsonObject getHierachyDetails(CSVRecord record,Map<String, Integer> headerMap){
+		JsonObject json = new JsonObject();
+		for(Map.Entry<String, Integer> header : headerMap.entrySet()){
+			if(header.getKey() != null && !DatataggingConstants.ACTION.equalsIgnoreCase(header.getKey())){
+					json.addProperty(header.getKey(), record.get(header.getValue()));
+			}
+		}
+		return json;
+	}
+	
+	private File convertToFile(MultipartFile multipartFile) throws IOException {
+		File file = new File(multipartFile.getOriginalFilename());
+		if(file.createNewFile()) { 
+			try(FileOutputStream fos = new FileOutputStream(file) ){ 
+				fos.write(multipartFile.getBytes());
+			}
+		}
+		return file;
+	}
+
+}
