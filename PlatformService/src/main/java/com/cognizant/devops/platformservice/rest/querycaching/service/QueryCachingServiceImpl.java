421c50785e86d2aab48ecc6b69d97b983a60a6c9
Akshay Sharma <Akshay.Sharma4@cognizant.com>
2018-04-13 13:22:26 +0530

    Added Apache License in files

--- PlatformService/src/main/java/com/cognizant/devops/platformservice/rest/querycaching/service/QueryCachingServiceImpl.java
+++ PlatformService/src/main/java/com/cognizant/devops/platformservice/rest/querycaching/service/QueryCachingServiceImpl.java
@@ -1,3 +1,18 @@
+/*******************************************************************************
+ * Copyright 2017 Cognizant Technology Solutions
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License.  You may obtain a copy
+ * of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ ******************************************************************************/
 package com.cognizant.devops.platformservice.rest.querycaching.service;
 
 import javax.ws.rs.core.MediaType;
@@ -64,14 +79,6 @@ public class QueryCachingServiceImpl implements QueryCachingService {
 	private JsonObject getEsCachedResults(String requestPayload) {
 
 		try {
-
-			// Pull cypher query, start time, end time, cacheNocache variable out of json
-			// If noCache= true, directly hit neo4j and return result
-			// Create hash out of cypherquery
-			// Query ES databasse based on hash and start and end time
-			// if result present, return to UI
-			// Query Neo4j get result
-			// Save result with hashcode and time field in ES
 			JsonParser parser = new JsonParser();
 			JsonObject requestJson = parser.parse(requestPayload).getAsJsonObject();
 
