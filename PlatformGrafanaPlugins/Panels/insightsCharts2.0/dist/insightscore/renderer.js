cc74ed8f515ca4663835fb4e15ab63f0b219e97d
vganjare <vishal.r.ganjare@gmail.com>
2018-05-07 15:23:01 -0500

    Fixing InSights charts for unwanted "dashboard" save pop-up (when dashboard is changed)

---
+++ PlatformGrafanaPlugins/Panels/insightsCharts2.0/dist/insightscore/renderer.js
@@ -0,0 +1,16 @@
+///<reference path="../../node_modules/grafana-sdk-mocks/app/headers/common.d.ts" />
+System.register([], function(exports_1) {
+    var InsightsCoreRenderer;
+    return {
+        setters:[],
+        execute: function() {
+            InsightsCoreRenderer = (function () {
+                function InsightsCoreRenderer() {
+                }
+                return InsightsCoreRenderer;
+            })();
+            exports_1("InsightsCoreRenderer", InsightsCoreRenderer);
+        }
+    }
+});
+//# sourceMappingURL=renderer.js.map
