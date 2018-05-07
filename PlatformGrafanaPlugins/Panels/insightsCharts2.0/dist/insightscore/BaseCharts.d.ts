c311e8cff8fb3e2559282222ff721bde80f93433
vganjare <vishal.r.ganjare@gmail.com>
2018-05-07 15:23:01 -0500

    Fixing InSights charts for unwanted "dashboard" save pop-up (when dashboard is changed)

---
+++ PlatformGrafanaPlugins/Panels/insightsCharts2.0/dist/insightscore/BaseCharts.d.ts
@@ -0,0 +1,29 @@
+/// <reference path="../../node_modules/grafana-sdk-mocks/app/headers/common.d.ts" />
+import { ChartModel } from './ChartModel';
+export declare class BaseCharts {
+    private chartModel;
+    static supportedDataTypes: string[];
+    private chartEditorWrapper;
+    constructor(chartModel: ChartModel);
+    /**
+     * Check if the google charts library is loaded or not. If not, then add new script tag.
+     */
+    loadGoogleCharts(): void;
+    getChartEditorWarpper(): any;
+    renderChart(isEditChart: boolean): void;
+    private transformData(data);
+    private joinDataTables(dataTables);
+    private buildDataTables(dataArray);
+    /**
+     * Use executeRenderChart for peoperly configured chart options.
+     */
+    private executeRenderChart();
+    /**
+     * Use loadChartEditor for editing the chart and create proper chart options.
+     * Will mostly be used in editor panel
+     */
+    private executeEditChart();
+    appendChartContainer(): void;
+    private convertData(dataRows, typeMapping);
+    private convertToType(data, type);
+}
