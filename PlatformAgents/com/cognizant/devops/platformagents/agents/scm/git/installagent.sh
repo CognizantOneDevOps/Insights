cefd5cb4cc70f02dd4bfde3439a7e87c8ffb1d55
vishwajitmankar <vishwajit.mankar@cognizant.com>
2018-05-11 12:31:37 +0530

    Fixed Windows service update issue. Also added functionality to handle agent uninstallation

--- PlatformAgents/com/cognizant/devops/platformagents/agents/scm/git/installagent.sh
+++ PlatformAgents/com/cognizant/devops/platformagents/agents/scm/git/installagent.sh
@@ -1,30 +1,40 @@
 # Turn on a case-insensitive matching (-s set nocasematch)
 opt=$1
+action=$2
 echo "$opt"
 case $opt in
         [lL][Ii][nN][uU][Xx])
-                echo "Git Running on Linux..."
-				sudo cp -xp InSightsGitAgent.sh  /etc/init.d/InSightsGitAgent
-				sudo chmod +x /etc/init.d/InSightsGitAgent
-				sudo chkconfig InSightsGitAgent on
-				sudo service  InSightsGitAgent status
-				sudo service  InSightsGitAgent stop
-				sudo service  InSightsGitAgent status
-				sudo service  InSightsGitAgent start
-				sudo service  InSightsGitAgent status
-				
-				echo "Service installaton steps completed"
-                ;;
+                case $action in 
+                	[uU][nN][iI][nN][sS][tT][aA][lL][lL])
+                		sudo service  InSightsGitAgent stop
+						sudo rm -R /etc/init.d/InSightsGitAgent
+						echo "Service un-installation step completed"
+		            ;;
+		         	*)
+		         		echo "Git Running on Linux..."
+						sudo cp -xp InSightsGitAgent.sh  /etc/init.d/InSightsGitAgent
+						sudo chmod +x /etc/init.d/InSightsGitAgent
+						sudo chkconfig InSightsGitAgent on
+						sudo service  InSightsGitAgent status
+						sudo service  InSightsGitAgent stop
+						sudo service  InSightsGitAgent status
+						sudo service  InSightsGitAgent start
+						sudo service  InSightsGitAgent status
+						
+						echo "Service installaton steps completed"
+		        	;;   
+				esac
+		;;
         [uU][bB][uU][nN][tT][uU])
                 echo "Git Running on Ubuntu..."
-				sudo cp -xp InSightsGitAgent.service /etc/systemd/system
-				sudo systemctl enable InSightsGitAgent
-				sudo systemctl start InSightsGitAgent
-				echo "Service installaton steps completed"
-                ;;
+		sudo cp -xp InSightsGitAgent.service /etc/systemd/system
+		sudo systemctl enable InSightsGitAgent
+		sudo systemctl start InSightsGitAgent
+		echo "Service installaton steps completed"
+         ;;
         centos)
-                echo "Git Running on centso..."
-                ;;
+               echo "Git Running on centso..."
+        ;;
         *)
         	    echo "Please provide correct OS input"
 esac
