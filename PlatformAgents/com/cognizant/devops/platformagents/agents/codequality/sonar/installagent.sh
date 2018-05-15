4bbdb8a000de1033e894e300cea0670077e5404e
KalaivaniGaneshan <kalaivani.g@cognizant.com>
2018-05-15 12:35:48 +0000

    Updated Agents for Uninstallation action

--- PlatformAgents/com/cognizant/devops/platformagents/agents/codequality/sonar/installagent.sh
+++ PlatformAgents/com/cognizant/devops/platformagents/agents/codequality/sonar/installagent.sh
@@ -1,8 +1,16 @@
 # Turn on a case-insensitive matching (-s set nocasematch)
 opt=$1
+action=$2
 echo "$opt"
 case $opt in
         [lL][Ii][nN][uU][Xx])
+          case $action in 
+            [uU][nN][iI][nN][sS][tT][aA][lL][lL])
+	          	sudo service InSightsSonarAgent stop
+				sudo rm -R /etc/init.d/InSightsSonarAgent
+				echo "Service un-installation step completed"
+		        ;;
+		    *)
                 echo "SonarAgent Running on Linux..."
 				sudo cp -xp InSightsSonarAgent.sh  /etc/init.d/InSightsSonarAgent
 				sudo chmod +x /etc/init.d/InSightsSonarAgent
@@ -14,13 +22,23 @@ case $opt in
 				sudo service  InSightsSonarAgent status
 				echo "Service installed.."
                 ;;
+		  esac
+		  ;;
         [uU][bB][uU][nN][tT][uU])
+	      case $action in 
+             [uU][nN][iI][nN][sS][tT][aA][lL][lL]) 
+				sudo systemctl stop InSightsSonarAgent
+				sudo rm -R /etc/systemd/system/InSightsSonarAgent.service
+				echo "Service un-installation step completed"				
+			    ;;
+			 *)
                 echo "SonarAgent Running on Ubuntu..."
 				sudo cp -xp InSightsSonarAgent.service /etc/systemd/system
 				sudo systemctl enable InSightsSonarAgent
 				sudo systemctl start InSightsSonarAgent
 				echo "Service Installed..."
                 ;;
+		  esac
         centos)
                 echo "SonarAgent Running on centso..."
                 ;;
