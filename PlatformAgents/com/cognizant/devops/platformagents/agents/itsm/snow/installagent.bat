4db4d4170c470d3f3b65539922080e078c3e11df
KalaivaniGaneshan <33314756+KalaivaniGaneshan@users.noreply.github.com>
2018-05-16 09:15:43 +0530

    Updated Uninstall action

--- PlatformAgents/com/cognizant/devops/platformagents/agents/itsm/snow/installagent.bat
+++ PlatformAgents/com/cognizant/devops/platformagents/agents/itsm/snow/installagent.bat
@@ -1,10 +1,10 @@
 REM pushd %INSIGHTS_AGENT_HOME%\PlatformAgents\snow
 IF /I "%1%" == "UNINSTALL" (
-	net stop snowAgent
-	sc delete snowAgent
+	net stop SnowAgent
+	sc delete SnowAgent
 ) ELSE (
-	net stop snowAgent
-	sc delete snowAgent
-    nssm install snowAgent %INSIGHTS_AGENT_HOME%\PlatformAgents\snow\SnowAgent.bat
-    net start SnowAgent
+	net stop SnowAgent
+	sc delete SnowAgent
+        nssm install SnowAgent %INSIGHTS_AGENT_HOME%\PlatformAgents\snow\SnowAgent.bat
+        net start SnowAgent
 )
