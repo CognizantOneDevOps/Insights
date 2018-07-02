bd90d8b1251e676d82cceeb688e5a8f110a6303a
DhrubajyotiDebnathcts <Dhrubajyoti.Debnath3@cognizant.com>
2018-07-02 11:11:43 +0000

    Updating datasource,query.editor.html,isight.js, Elasticsearchservice.java for grafna 5.0 upgrade.

--- PlatformGrafanaPlugins/DataSources/neo4j2.0/src/datasource.ts
+++ PlatformGrafanaPlugins/DataSources/neo4j2.0/src/datasource.ts
@@ -133,26 +133,54 @@ export default class Neo4jDatasource {
 
   executeCypherQuery(cypherQuery, targets, options) {
     var deferred = this.$q.defer();
-    var self = this;
-    this.backendSrv.datasourceRequest({
-      url: this.url,
-      method: 'POST',
-      data: JSON.stringify(cypherQuery)
-    }).then(function (response) {
-      var data = response.data;
-      if (response.status === 200) {
-        if (data && data.results && data.results.length > 0) {
-          data['targets'] = targets;
-          deferred.resolve({ data: self.processResponse(data, options) });
-        } else {
-          deferred.resolve({ status: "success", message: "No data returned", title: "success" });
-        }
-      } else {
-        deferred.resolve({ status: "failure", message: "Unable to connect to Datasource", title: "Failure" });
-      }
-    });
+	var flag = this.checkCypherQueryModificationKeyword(cypherQuery);
+	if( flag == true){
+		
+		options.targets[0].checkQuery = false;
+		var self = this;
+		this.backendSrv.datasourceRequest({
+		  url: this.url,
+		  method: 'POST',
+		  data: JSON.stringify(cypherQuery)
+		}).then(function (response) {
+		  var data = response.data;
+		  if (response.status === 200) {
+			if (data && data.results && data.results.length > 0) {
+			  data['targets'] = targets;
+			  deferred.resolve({ data: self.processResponse(data, options) });
+			} else {
+			  deferred.resolve({ status: "success", message: "No data returned", title: "success" });
+			}
+		  } else {
+			deferred.resolve({ status: "failure", message: "Unable to connect to Datasource", title: "Failure" });
+		  }
+		});
+	
+  }
+	else{
+		
+		options.targets[0].checkQuery = true;
+		deferred.resolve({ status: "failure", message: "Cannot run modification query in neo4j", title: "Failure" });
+		console.log("It has create//delete/set/update keyword.");
+	}
     return deferred.promise;
   }
+checkCypherQueryModificationKeyword(cypherQuery){
+	var keywords:string[]; 
+	keywords = ["create","delete","set","update"];
+	var flag:number = 0;
+	var queryCorrect = true;
+	var j;
+	for(j in keywords){
+		var query = (cypherQuery.statements[0].statement.toString()).toLowerCase();
+		if( query.indexOf(keywords[j]) >= 0){flag = 1;break;}
+		}
+	if( flag == 0){return queryCorrect;}
+	else{
+		queryCorrect = false;
+		return queryCorrect
+	}
+}
 
   //let templateName = variable.model.name;
   //Two places where the templates can be used:
