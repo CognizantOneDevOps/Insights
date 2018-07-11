e007040f3349d0314bed4282b4454d72591f73ed
DhrubajyotiDebnathcts <Dhrubajyoti.Debnath3@cognizant.com>
2018-07-11 09:52:02 +0000

    adding installion script changes for ubuntu. This is only for installation scripts. Not added agent2.0 changes to agent-install scripts.

---
+++ Deployment/UBUNTU/insights_es.sh
@@ -0,0 +1,35 @@
+#-------------------------------------------------------------------------------
+# Copyright 2017 Cognizant Technology Solutions
+#   
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not
+# use this file except in compliance with the License.  You may obtain a copy
+# of the License at
+# 
+#   http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+# License for the specific language governing permissions and limitations under
+# the License.
+#-------------------------------------------------------------------------------
+# install ES  - Some Issues here test on new server
+echo "#################### Installing Eleastic Search with configs ####################"
+mkdir elasticsearch
+cd elasticsearch
+#wget http://platform.cogdevops.com/insights_install/installationScripts/latest/RHEL/es/elasticsearch-5.6.4.rpm
+#sudo rpm -Uvh elasticsearch-5.6.4.rpm
+#sudo wget http://platform.cogdevops.com/insights_install/installationScripts/latest/RHEL/es/ElasticSearch-5.6.4.zip
+#sudo unzip ElasticSearch-5.6.4.zip
+#sudo cp ElasticSearch-5.6.4/elasticsearch.yml /etc/elasticsearch/elasticsearch.yml
+#sudo cp ElasticSearch-5.6.4/log4j2.properties /etc/elasticsearch/log4j2.properties
+#sudo systemctl daemon-reload
+#sudo systemctl enable elasticsearch.service
+#sudo systemctl start elasticsearch.service
+curl -L -O https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-5.6.10.zip
+unzip elasticsearch-5.6.10.zip
+cd elasticsearch-5.6.10
+nohup ./bin/elasticsearch &
+sleep 20
+curl -XPUT 'localhost:9200/_template/neo4j-*' -d '{"order" : 0, "template": "neo4j-*","settings": {"index.number_of_shards": "5"},"mappings": {"_default_": {"dynamic_templates": [{"string_fields" : {"mapping" : {"index" : "analyzed","omit_norms" : true,"type" : "string","fields" : {"raw" : {"ignore_above" : 256,"index" : "not_analyzed","type" : "string"}}},"match_mapping_type" : "string","match" : "*"}}]}},"aliases": {}}}'
+
---
+++ Deployment/UBUNTU/insights_java.sh
@@ -0,0 +1,45 @@
+#-------------------------------------------------------------------------------
+# Copyright 2017 Cognizant Technology Solutions
+#   
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not
+# use this file except in compliance with the License.  You may obtain a copy
+# of the License at
+# 
+#   http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+# License for the specific language governing permissions and limitations under
+# the License.
+#-------------------------------------------------------------------------------
+#  Copyright 2017 Cognizant Technology Solutions
+#
+#  Licensed under the Apache License, Version 2.0 (the "License"); you may not
+#  use this file except in compliance with the License.  You may obtain a copy
+#  of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+#  License for the specific language governing permissions and limitations under
+#  the License.
+# Install Java
+echo "#################### Installing Java with Env Variable ####################"
+cd /opt/
+wget  http://platform.cogdevops.com/insights_install/installationScripts/latest/RHEL/java/jdk-8u151-linux-x64.tar.gz
+tar xzf jdk-8u151-linux-x64.tar.gz
+export JAVA_HOME=/opt/jdk1.8.0_151
+echo JAVA_HOME=/opt/jdk1.8.0_151  | tee -a /etc/environment
+echo "export" JAVA_HOME=/opt/jdk1.8.0_151 | tee -a /etc/profile
+export JRE_HOME=/opt/jdk1.8.0_151/jre
+echo JRE_HOME=/opt/jdk1.8.0_151/jre | tee -a /etc/environment
+echo "export" JRE_HOME=/opt/jdk1.8.0_151/jre | tee -a /etc/profile
+export PATH=$PATH:/opt/jdk1.8.0_151/bin:/opt/jdk1.8.0_151/jre/bin
+echo PATH=$PATH:/opt/jdk1.8.0_151/bin:/opt/jdk1.8.0_151/jre/bin | tee -a /etc/environment
+alternatives --install /usr/bin/java java /opt/jdk1.8.0_151/bin/java 20000
+source /etc/environment
+source /etc/profile
+
