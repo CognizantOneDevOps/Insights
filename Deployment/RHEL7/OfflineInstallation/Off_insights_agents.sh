c202430c742af3fa7651019b0a6e25ff9be3646e
Chellamuthu <543825@Cognizant.com>
2018-08-14 13:08:54 +0530

    Added offline instalaion scripts to install without internet in Deployment/RHEL7/OfflineInstallation

---
+++ Deployment/RHEL7/OfflineInstallation/Off_insights_agents.sh
@@ -0,0 +1,36 @@
+#-------------------------------------------------------------------------------
+# Copyright 2017 Cognizant Technology Solutions
+#   
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not
+# use this file except in compliance with the License.  You may obtain a copy
+# of the License at
+# 
+#   http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+# License for the specific language governing permissions and limitations under
+# the License.
+#-------------------------------------------------------------------------------
+echo "Set up Agent_Daemon"
+cd /opt/ && mkdir insightsagents
+chmod -R 755 insightsagents/
+cd insightsagents
+export INSIGHTS_AGENT_HOME=`pwd`
+echo INSIGHTS_AGENT_HOME=`pwd` | tee -a /etc/environment
+echo "export" INSIGHTS_AGENT_HOME=`pwd` | tee -a /etc/profile
+sudo mkdir PlatformAgents
+chmod -R 755 PlatformAgents
+echo $INSIGHTS_AGENT_HOME
+sudo cp -R /usr/Offline_Installation/Insight_Components/agentdaemon ./
+cd agentdaemon
+source /etc/environment
+source /etc/profile
+sudo sed -i -e "s|extractionpath|/opt/insightsagents/PlatformAgents|g" /opt/insightsagents/agentdaemon/com/cognizant/devops/platformagents/agents/agentdaemon/config.json
+sudo chmod +x installdaemonagent.sh
+sudo mkdir /opt/agent20
+sudo mkdir /opt/agent20/download
+sudo chmod -R 777 /opt/agent20
+sh ./installdaemonagent.sh Linux
+
