7483bec4ab1bc25a4cc776ac2df5c68aa6b053f8
DhrubajyotiDebnathcts <Dhrubajyoti.Debnath3@cognizant.com>
2018-08-14 13:51:28 +0000

    Adding changes in datasource.ts for any inner word set present as modification query.

--- PlatformGrafanaPlugins/DataSources/neo4j2.0/src/datasource.ts
+++ PlatformGrafanaPlugins/DataSources/neo4j2.0/src/datasource.ts
@@ -174,7 +174,7 @@ export default class Neo4jDatasource {
     var j;
     for (j in keywords) {
       var query = (cypherQuery.statements[0].statement.toString()).toLowerCase();
-      if (query.indexOf(keywords[j]) >= 0) { flag = 1; break; }
+      if (query.indexOf( " "+keywords[j] ) >= 0 || query.indexOf( keywords[j] + " " ) >= 0 || query.indexOf( ")"+keywords[j] ) || query.indexOf( keywords[j] + "(" ) >= 0) { flag = 1; break; }
     }
     if (flag == 0) { return queryCorrect; }
     else {
