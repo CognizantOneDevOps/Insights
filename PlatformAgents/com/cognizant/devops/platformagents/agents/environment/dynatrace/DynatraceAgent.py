b88e2be49743cc7f073a55f8e0b65c45a8ebf2f1
vishwajitmankar <vishwajit.mankar@cognizant.com>
2018-09-25 16:16:04 +0530

    First check-in for Dynatrace agent

---
+++ PlatformAgents/com/cognizant/devops/platformagents/agents/environment/dynatrace/DynatraceAgent.py
@@ -0,0 +1,50 @@
+#-------------------------------------------------------------------------------
+# Copyright 2017 Cognizant Technology Solutions
+# 
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not
+# use this file except in compliance with the License.  You may obtain a copy
+# of the License at
+# 
+#   http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+# License for the specific language governing permissions and limitations under
+# the License.
+#-------------------------------------------------------------------------------
+'''
+Created on Sep 24, 2018
+
+@author: Vishwajit Mankar & Gaurav Deshmukh
+'''
+from dateutil import parser
+import time
+import sys
+import os
+from datetime import datetime, timedelta
+from com.cognizant.devops.platformagents.core.BaseAgent import BaseAgent
+import logging.handlers
+
+class DynatraceAgent(BaseAgent):
+    def process(self):
+        print("Inside process")
+        try:
+            self.apiToken = self.config.get("apiToken", None)
+            startFrom = self.config.get("startFrom", None)
+            startFrom = parser.parse(startFrom)
+            responseTemplate = self.getResponseTemplate()
+            hostURL = responseTemplate.get('hostInfo').get('hostListURL')
+            gethostUrlWithToken = hostURL+"?Api-Token="+self.apiToken
+            repos = self.getResponse(gethostUrlWithToken+'&per_page=100&sort=created&page=1', 'GET', None, None, None)
+            print(repos)
+        except Exception as e:
+            logging.error(e)
+            
+        tracking_data = []
+        
+        if tracking_data!=[]:
+            self.publishToolsData(tracking_data)
+        
+if __name__ == "__main__":
+    DynatraceAgent()
