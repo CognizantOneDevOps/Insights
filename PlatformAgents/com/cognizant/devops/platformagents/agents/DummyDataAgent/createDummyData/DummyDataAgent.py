1d0db3e71a977fd9a75a7bfc9f722974a7a2fe38
Chellamuthu <543825@Cognizant.com>
2018-10-01 15:31:39 +0530

    Added variables in DummyDataAgent For corelated dashboard Demo, and added sample corelated dashboards

--- PlatformAgents/com/cognizant/devops/platformagents/agents/DummyDataAgent/createDummyData/DummyDataAgent.py
+++ PlatformAgents/com/cognizant/devops/platformagents/agents/DummyDataAgent/createDummyData/DummyDataAgent.py
@@ -45,7 +45,9 @@ class DummyDataAgent(BaseAgent):
         "jiraKey":"KKT-3",
         "inSightsTime":1473777524,
         "sprint":"Sprint4",
+        "fixVersions": "ACS17.0.4.3"
         }
+
         
         #GIT sample json
         gitSample = {
@@ -76,34 +78,90 @@ class DummyDataAgent(BaseAgent):
           "toolName": "JENKINS",
           "projectID": "1002"
         }
-                
+
+        #Sonar Sample json
+        sonarSample= {
+            "id": 4,
+            "k": "PaymentServices",
+            "nm": "PaymentServices",
+            "sc": "PRJ",
+            "qu": "TRK"
+         }
+        sprintSample = {
+            "sprintName":"Adoption",
+            "sprintId":"ad1",
+            "state":"closed"
+            }
+        
         #Jira variables
-        jira_status = ['Open','Backlog','To Do','In Progress']
+        jira_status = ['Open','Backlog','To Do','In Progress', 'Canceled', 'Done','Closed']
         jira_priority = ['Low','Medium','High']
-        jir_issuetype = ['Story','Task','Sub-task','Bug','Epic']
+        jira_issuetype = ['Story','Task','Sub-task','Bug','Epic','User Story']
         jira_creator = ['Akshay','Mayank','Vishwajit','Prajakta','Vishal']
         jira_sprint = ['S51','S52','S53','S54','S55']
         jira_project_name = ['PaymentServices','MobileServices','ClaimFinder','AgentLocator']
-        jira_key = ['IS-10','IS-11','IS-12','IS-13']
-        
+        jira_key = ['IS-10','IS-11','IS-12','IS-13','IS-14']
+        Story_Id = ['IS-10','IS-11','IS-12','IS-13','IS-14']
+        jira_version = ['ACS17.0.4.3','BDE17.0.4.3','ACS19.0.3.1']
+	sprints = ['1','2','3','4']	
+	state = ['start', 'closed','finish','deliver']
+	Priority = ['2','3','4','5']
+	
+	Author_Name = ['HAri', 'Dhrubaj','Akshay','Tommy']
+
+	resolution = ['Done','Completed']
+	storyPoints = ['1','3','5','7']
+	alm_ID = ['a23','a33','a44','a55']
+	progressTimeSec = ['1232','32342','2323']
+	assigneeID  = ['1231212','2345253','234234','1342323']
+	assigneeEmail = ['hari@cognizant.com','sashikala@cognizant.com','drubaj@cognizant.com','kalaivani@cognizant.com']
+        Sprint_Id = ['1','2','3','4']
+
+
+		
+	#Sprint variables
+
+	sprint_Name = ['Adoption','UIEnhance','Three','Testphase']
+	sprint_Id = ['1','2','3','4','5','6']
+	state = ['start', 'closed','finish','deliver']
+	issue_Type = ['Bug', 'Sprint Bug','SIT Bug','Performance Bug','Regression Bug']
+	
+		
         #GIT variables
         repo = ['Insights','InsightsDemo','InsightsTest','InsightsTest']
         author = ['Akshay','Mayank','Vishwajit','Prajakta']
-        
+        message = ['Adding debug lines for IS-10 / S51', 'Remvoing bug for IS-11 / S52', 'New feature added for IS-1 / S53', 'Rolling back changes IS-13 / S54']
+	Commit_Id =  ['123','456','789','111','009','008','007','990']
+		
+       
         #Jenkins variables
         sprint = ['S51','S52','S53','S54','S55']
         status = ['Success','Failure','Aborted']
         project_name = ['PaymentServices','MobileServices','ClaimFinder','AgentLocator']
-        job_name = ['BillingApproved','BillingInvoice','ClaimValidated','ClaimProcessed']
+        job_name = ['BillingApproved','BillingInvoice','ClaimValidated','ClaimProcessed','deploy']
         projectId = ['1001','1002','1003','1004']
-        jen_env = ['PROD','DEV','INT']
-        
+        jen_env = ['PROD','DEV','INT', 'RELEASE']
+	buildUrl =  ['productv4.1.devops.com','productv4.2.devops.com','productv4.3.devops.com','productv4.4.devops.com']
+	result = ['SUCCESS','FAILURE','ABORTED']
+	master = ['master1','master2']
+	SCM_commitID = ['123','456','789','111']
+	
+
+
+        #Sonar variables
+        project = ['PaymentServices','MobileServices','ClaimFinder','AgentLocator']
+        sonar_key = ['payment1','Mobile1', 'Claim', 'agent']
+        project_id =['1','2','3','4']
+        resourceKey = ['09','099','89','32']
+	
+
         count = self.tracking.get("dataCount")
         flag = 1
         
         #To save the data count in tracking.json
         script_dir = os.path.dirname(__file__)
-        file_path = os.path.join(script_dir, '~/tracking.json') #Input your system path to tracking.json of DummyAgent          
+        file_path = os.path.join('C:\\InSights_V1.0\\Agents\\PlatformAgents\\com\\cognizant\\devops\\platformagents\\agents\\ci\\test1\\tracking.json')
+        #Input your system path to tracking.json of DummyAgent          
         with open(file_path, "r") as jsonFile: # Open the JSON file for reading
             data = json.load(jsonFile) # Read the JSON into the buffer
             
@@ -111,8 +169,10 @@ class DummyDataAgent(BaseAgent):
                 
         while flag == 1:
             jira_data = []
+            sprint_data = []
             git_data = []
             jenkins_data = []
+            sonar_data = []
             
             #Run-time calculated variables
             currentDT = datetime.datetime.now()
@@ -121,24 +181,46 @@ class DummyDataAgent(BaseAgent):
             randomStr = ''.join([random.choice(string.ascii_letters + string.digits) for n in xrange(32)])
             time_start = (random.randint(100, 500))
             time_end = (random.randint(501, 800))
-            
+      
             #Jira json configurations
             jiraSample['inSightsTimeX'] = currentDT.strftime("%Y-%m-%d"+'T'+"%H:%M:%S"+'Z')
             jiraSample['jiraUpdated'] = currentDT.strftime("%Y-%m-%d"+'T'+"%H:%M:%S"+'Z')
             jiraSample['inSightsTime'] = time_epoch
-            jiraSample['jiraProjectName'] = random.choice(jira_project_name)
             jiraSample['jiraCreator'] = random.choice(jira_creator)
             jiraSample['jiraPriority'] = random.choice(jira_priority)
-            jiraSample['jiraIssueType'] = random.choice(jir_issuetype)
-            jiraSample['jiraKey'] = random.choice(jira_key)
+            jiraSample['jiraIssueType'] = random.choice(jira_issuetype)
             jiraSample['sprint'] = random.choice(jira_sprint)
-            jiraSample['jiraStatus'] = random.choice(jira_status)
-            
-            jira_data.append(jiraSample)
-            #print jira_data
-            self.publishToolsData(jira_data)
-            
-            
+            jiraSample['jiraStatus'] = random.choice(jira_status)            
+            jiraSample['fixVersions'] = random.choice(jira_version)			
+            jiraSample['sprints'] = random.choice(sprints)
+            jiraSample['sprints1'] = random.choice(sprints1)
+            jiraSample['issueType'] = random.choice(issue_Type)
+            jiraSample['key'] = random.choice(jira_key)
+            jiraSample['storyId'] = random.choice(Story_Id)
+	    jiraSample['Priority'] = random.choice(Priority)
+            jiraSample['status'] = random.choice(status)
+            jiraSample['projectName'] = random.choice(jira_project_name)
+            jiraSample['resolution'] = random.choice(resolution)
+	    jiraSample['storyPoints'] = random.choice(storyPoints)
+	    jiraSample['almID'] = random.choice(alm_ID)
+	    jiraSample['progressTimeSec'] = random.choice(progressTimeSec)
+	    jiraSample['assigneeID'] = random.choice(assigneeID)
+	    jiraSample['assigneeEmail'] = random.choice(assigneeEmail)
+            jiraSample['authorName'] = random.choice(Author_Name)
+            jiraSample['sprintId'] = random.choice(Sprint_Id)
+	    jira_data.append(jiraSample)
+            #self.publishToolsData(jira_data)
+
+            #sprint json configurations	            
+            sprintSample['sprintName'] = random.choice(sprint_Name)
+	    sprintSample['sprintId'] = random.choice(sprint_Id)
+	    sprintSample['state'] = random.choice(state)
+            sprintSample['issueType'] = random.choice(issue_Type)
+            sprint_data.append(sprintSample)
+            metadata = {"labels" : ["SPRINT"]}
+            #self.publishToolsData(sprint_data, metadata)
+        
+
                         
             #GIT json configurations
             git_time = (time_epoch + time_start)
@@ -150,10 +232,12 @@ class DummyDataAgent(BaseAgent):
             gitSample['jiraKey'] = jiraSample['jiraKey']
             gitSample['gitReponame'] = random.choice(repo)
             gitSample['gitAuthorName'] = random.choice(author)
-                        
+            gitSample['repoName'] = random.choice(repo)
+            gitSample['message'] = random.choice(message)
+	    gitSample['commitId'] = random.choice(Commit_Id)
+			
             git_data.append(gitSample)
-            #print git_data
-            self.publishToolsData(git_data)
+            #self.publishToolsData(git_data)
             
             #Jenkins json configurations
             jenkins_date = (git_date+datetime.timedelta(seconds=time_end))
@@ -172,12 +256,35 @@ class DummyDataAgent(BaseAgent):
             jenkinsSample['projectName'] = random.choice(project_name)
             jenkinsSample['projectID'] = random.choice(projectId)
             jenkinsSample['environment'] = random.choice(jen_env)
-            
+	    jenkinsSample['buildUrl'] = random.choice(buildUrl)
+	    jenkinsSample['result'] = random.choice(result)
+	    jenkinsSample['master'] = random.choice(master)
+	    jenkinsSample['scmcommitId'] = random.choice(SCM_commitID)
+			   
+		
             jenkins_data.append(jenkinsSample)
             #print jenkins_data
-            self.publishToolsData(jenkins_data)
+            #self.publishToolsData(jenkins_data)
+
             
+            #Sonar jenkins configuration
+	    sonar_date = (jenkins_date+datetime.timedelta(seconds=time_end))
+	    sonar_startTime = gitSample['inSightsTime'] + time_start
+            sonar_endTime = gitSample['inSightsTime'] + time_end
+	    sonarSample['inSightsTimeX'] = sonar_date.strftime("%Y-%m-%d"+'T'+"%H:%M:%S"+'Z')
+            sonarSample['inSightsTime'] = sonar_endTime
+            sonarSample['projectname']= random.choice(project)
+            sonarSample['ProjectID']= random.choice(project_id)
+            sonarSample['ProjectKey']= random.choice(sonar_key)
+            sonarSample['resourceKey']= random.choice(resourceKey)
+	    sonarSample['inSightsTime']= random.choice(resourceKey)
+	    sonarSample['inSightsTimeX']= random.choice(resourceKey)
             
+            sonar_data.append(sonarSample)
+            self.publishToolsData(sonar_data)
+			
+    
+        
             print 'Published data: ',count
             count +=1
                        
@@ -185,7 +292,7 @@ class DummyDataAgent(BaseAgent):
             tmp = data["dataCount"] 
             data["dataCount"] = count
         
-             #To save the data count in tracking.json   
+            #To save the data count in tracking.json   
             ## Save our changes to JSON file
             with open(file_path, "w") as jsonFile:
                 #print 'Writing'
