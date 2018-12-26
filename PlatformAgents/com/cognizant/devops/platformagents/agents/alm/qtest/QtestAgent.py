3be547de82322b2ad70ee26c8d8ba46d47bd8449
DhrubajyotiDebnathcts <36297783+DhrubajyotiDebnathcts@users.noreply.github.com>
2018-12-26 11:29:25 +0530

    Q test agent update (#193)
    * #162373795 Adding test-run expand, properties from api-response and jira key extraction.
    
    * #162373795 Adding requirements to test-case relationship.
    
    * #162373795 Adding exception for rest api response fetch.
    
    * #162373795 Adding exception for rest api response fetch.
    
    * #162373795 Adding type requirement.
    
    * #162373795 Fixing typo config testCaseId to id.
    
    * #162373795 Adding related node logic between test-case, requirement, defects and test-runs in extension part.
    
    * #162373795 Adding related node logic between test-case, requirement, defects and test-runs in extension part.
    
    * #162373795 Optimizing the code as per extension logic.
    
    * #162373795 Adding extension logic for requirement matrix, linked artifacts..

--- PlatformAgents/com/cognizant/devops/platformagents/agents/alm/qtest/QtestAgent.py
+++ PlatformAgents/com/cognizant/devops/platformagents/agents/alm/qtest/QtestAgent.py
@@ -30,12 +30,14 @@ class QtestAgent(BaseAgent):
         password = self.config.get("password", None)
         startFrom = self.config.get("startFrom", '')
         timeStampFormat = self.config.get("timeStampFormat", '')
+        extensions = self.config.get('dynamicTemplate', {}).get('extensions', None)
         startFrom = parser.parse(startFrom, ignoretz=True)
         domainName = "InSightsAlmAgent:"
-        authToken = base64.b64encode(domainName.encode('utf-8'))
-        token = self.login(authToken, username, password, baseUrl)
-        headers = {"accept": "application/json","Authorization": "bearer "+token}
+        self.authToken = base64.b64encode(domainName.encode('utf-8'))
+        self.token = self.login(self.authToken, username, password, baseUrl)
+        headers = {"accept": "application/json","Authorization": "bearer "+self.token}
         pagination = ["test-cases", "requirements", "test-runs", "trace-matrix-report", "defects"]
+        self.username , self.password , self.headers , self.baseUrl = username, password, headers, baseUrl
         #In this part we addthe module name where pagination is supported.
         try:
             projectsList = self.getResponse(baseUrl+"/api/v3/projects?assigned=false", 'GET', None, None, None, None, headers)
@@ -53,17 +55,26 @@ class QtestAgent(BaseAgent):
                         entityUpdatedDate = None
                         for entityType in almEntities:
                             page_num = 1
-                            page_size = 25
+                            page_size = 10
                             data = []
+                            metadata = self.config.get("dynamicTemplate", {}).get("almEntityMetaData", None)
                             almEntityRestDetails = self.almEntityRestDetails(entityType, projectId, baseUrl, pagination)
                             entityUpdatedDate = almEntityRestDetails.get('entityUpdatedDate', None)
                             if entityUpdatedDate is not None:
                                 startFrom = parser.parse(entityUpdatedDate, ignoretz=True)
                             nextPageResponse = True
                             entity_type_available = False
+                            reqIdList = trackingDetails.get(entityType, {}).get("entityIdDict", None)
+                            if reqIdList is None:
+                                reqIdList = []
                             while nextPageResponse:
-                                restUrl = almEntityRestDetails.get('restUrl', None) + almEntityRestDetails.get('entityType', None) + "?expandProps=false&expandSteps=false&page=" + str(page_num) + "&size=" + str(page_size) + almEntityRestDetails.get('dateTimeStamp', None) + urllib.quote_plus(startFrom.strftime(timeStampFormat)) + "Z"
-                                entityTypeResponse = self.getResponse(restUrl, 'GET', None, None, None, None, headers)
+                                restUrl = almEntityRestDetails.get('restUrl', None) + almEntityRestDetails.get('entityType', None) + "?expandProps=true&expandSteps=false&expand=descendants&page=" + str(page_num) + "&size=" + str(page_size) + almEntityRestDetails.get('dateTimeStamp', None) + urllib.quote_plus(startFrom.strftime(timeStampFormat)) + "Z"
+                                try:
+                                    entityTypeResponse = self.getResponse(restUrl, 'GET', None, None, None, None, headers)
+                                except Exception as ex1:
+                                    nextPageResponse = False
+                                    logging.error("ProjectID: " + str(projectId) + " Type: " +str(entityType) + " URL: " + str(restUrl) + "  " + str(ex1))
+                                    break
                                 if entityType in pagination and "items" in entityTypeResponse and len(entityTypeResponse["items"]) == 0:
                                     break
                                 elif entityType in pagination and "items" in entityTypeResponse and len(entityTypeResponse["items"]) > 0:
@@ -86,27 +97,22 @@ class QtestAgent(BaseAgent):
                                                         injectData['projectName'] = projectName
                                                         injectData['projectId'] = projectId
                                                         injectData['almType'] = entityType
+                                                        #EXTRACTION OF JIRA-KEY FROM NAME FIELD IN REQUIREMENTS.
+                                                        if entityType == 'requirements':
+                                                            if 'name' in res:
+                                                                #matchObj = re.match( r'(.*)-(.*?) .*', res.get('name', ''), re.M|re.I)
+                                                                #injectData['jiraKey'] = '-'.join(matchObj.group(1, 2))
+                                                                #FOR NOW ASSUMING THAT IN NAME FIELD FIRST WORD WILL BE JIRA KEY.
+                                                                injectData['jiraKey'] = res.get('name', '').split(' ')[0]
+                                                        #EXTRACTION PROPERTY VALUES FROM API RESPONSE.
+                                                        if str(res.get('id')) not in reqIdList:
+                                                            reqIdList.append(str(res.get('id')))
+                                                        if 'properties' in res:
+                                                            for property in res.get('properties', []):
+                                                                injectData[str(property.get('field_name').lower()).replace(' ', '')] = property.get('field_value')
                                                         data += self.parseResponse(responseTemplate, res, injectData)
-                                            #Trace matrix do not support date time format. This is why it is outside if else.
-                                            if entityType == "trace-matrix-report":
-                                                dataMatrix = []
-                                                traceMatrixReport = res.get("requirements", {})
-                                                parentId = res.get("id", None)
-                                                parentName = res.get("name", None)
-                                                for matrix in traceMatrixReport:
-                                                    injectData= {}
-                                                    if "testcases" in matrix:
-                                                        injectData['projectName'] = projectName
-                                                        injectData['parentId'] = parentId
-                                                        injectData['parentName'] = parentName
-                                                        injectData['projectId'] = projectId
-                                                        injectData['almType'] = "trace-matrix-report"
-                                                        injectData['testcases'] = matrix.get("testcases", None)
-                                                        injectData['linkedTestCases'] = matrix.get("linked-testcases", None)
-                                                        injectData['pid'] = matrix.get("id", None)
-                                                        dataMatrix.append(injectData)
-                                                if len(dataMatrix) > 0:
-                                                    self.publishToolsData(dataMatrix, metadata)
+                                                        #FOR COLLECTING DATA BEYOND PARENT/ROOT LEVEL. FUNCTION DEFINITION IS AT THE BOTTOM.
+                                                        #self.injectResponseData(data, responseTemplate, res, projectName, projectId, entityType)
                                     except Exception as ex:
                                         nextPageResponse = False
                                         entity_type_available = False
@@ -116,20 +122,22 @@ class QtestAgent(BaseAgent):
                                     nextPageResponse = False
                                 if almEntityRestDetails.get('pagination', False):
                                     if entityType == "defects":
-                                        page_size = page_size + 25
+                                        page_size = page_size + 10
                                     else:
                                         page_num = page_num + 1
                                 else:
                                     nextPageResponse = False
                             if entity_type_available and entityUpdatedDate is not None:
-                                trackingDetails[entityType] = {"entityUpdatedDate": entityUpdatedDate}
-                            metadata = self.config.get("dynamicTemplate", {}).get("almEntityMetaData", None)
+                                if len(reqIdList) > 0 and entityType in extensions.get('linkedArtifacts', {}).get('almEntities', {}):
+                                    trackingDetails[entityType] = {"entityUpdatedDate": entityUpdatedDate, "entityIdDict": reqIdList}
+                                else:
+                                    trackingDetails[entityType] = {"entityUpdatedDate": entityUpdatedDate}
                             if len(data) > 0:
                                 self.publishToolsData(data, metadata)
                         self.tracking[str(projectId)] = trackingDetails
                         self.updateTrackingJson(self.tracking)                        
         finally:
-            self.logout(token, baseUrl)
+            self.logout(self.token, baseUrl)
     def login(self, authToken, username, password, baseUrl):
         headers_token = {'accept': "application/json",'content-type': "application/x-www-form-urlencoded",'authorization': "Basic "+str(authToken)+""}
         payload = "grant_type=password&username="+str(username)+"&password="+str(password)
@@ -161,6 +169,124 @@ class QtestAgent(BaseAgent):
             entityRestDetails['entityType'] = str(urlExtension.get(entityType, ""))
         if entityType in paginationList:
             entityRestDetails['pagination'] = True
-        return entityRestDetails           
+        return entityRestDetails
+    def scheduleExtensions(self):
+        extensions = self.config.get('dynamicTemplate', {}).get('extensions', None)
+        if extensions:
+            linkedArtifacts = extensions.get('linkedArtifacts', None)
+            if linkedArtifacts:
+                self.registerExtension('linkedArtifacts', self.retrieveLinkedArtifacts, linkedArtifacts.get('runSchedule'))
+            requirementMatrix = extensions.get('requirementMatrix', None)
+            if requirementMatrix:
+                self.registerExtension('requirementMatrix', self.retrieveRequirementMatrix, requirementMatrix.get('runSchedule'))
+    def retrieveLinkedArtifacts(self):
+        try:
+            token = self.login(self.authToken, self.username , self.password , self.baseUrl)
+            headers = {"accept": "application/json","Authorization": "bearer "+token}
+            linkedArtifacts = self.config.get('dynamicTemplate', {}).get('extensions', {}).get('linkedArtifacts', None)
+            trackingDetails = self.tracking
+            try:
+                for project in trackingDetails:
+                    data = []
+                    for almEntity in trackingDetails.get(str(project), {}):
+                        if almEntity in linkedArtifacts.get('almEntities', None):
+                            projectTrackingDetails = trackingDetails.get(str(project))
+                            entityIdDict = projectTrackingDetails.get(almEntity, {}).get('entityIdDict', None)
+                            if entityIdDict is None:
+                                continue
+                            try:
+                                dictIsNotEmpty = True
+                                start = 0
+                                end = 15
+                                while dictIsNotEmpty:
+                                    linkedArtifactUrl = "{}/api/v3/projects/{}/linked-artifacts?type={}&ids={}".format(self.baseUrl, str(project), almEntity, ','.join(entityIdDict[start:end]))
+                                    entityTypeResponse = self.getResponse(linkedArtifactUrl, 'GET', None, None, None, None, headers)
+                                    for res in entityTypeResponse:
+                                        parentId = res.get('id', None)
+                                        if 'objects' in res:
+                                            injectData = {}
+                                            injectData['id'] = parentId
+                                            injectData['projectId'] = int(project)
+                                            injectData['almType'] = almEntity
+                                            for object in res.get('objects', None):                                        
+                                                almType = (object.get('self', None).split('/')[-2]).replace('-', '')
+                                                if almType not in injectData:
+                                                    injectData[almType] = [object.get('id')]
+                                                else:
+                                                    injectData[almType].append(object.get('id', None)) 
+                                            data.append(injectData)
+                                    start = end
+                                    end = end + 15
+                                    if len(entityIdDict[start:end]) == 0:
+                                        dictIsNotEmpty = False
+                            finally:
+                                self.tracking.get(str(project)).get(almEntity, {}).pop('entityIdDict')
+                    if len(data) > 0:
+                        metadata = self.config.get("dynamicTemplate", {}).get('extensions', {}).get('linkedArtifacts', {}).get("almEntityMetaData", None)
+                        self.publishToolsData(data, metadata)
+                        self.updateTrackingJson(self.tracking)
+            except Exception as ex1:
+                logging.error("Error in retrieveLinkedArtifacts which is part of extensions in " + str(ex1))
+        finally:
+            self.logout(token, self.baseUrl)
+
+    def retrieveRequirementMatrix(self):
+        dataMatrix = []
+        metadata = self.config.get("dynamicTemplate", {}).get('extensions', {}).get('requirementMatrix', {}).get("almEntityMetaData", None)
+        try:
+            token = self.login(self.authToken, self.username , self.password , self.baseUrl)
+            headers = {"accept": "application/json","Authorization": "bearer "+token}
+            trackingDetails = self.tracking
+            for project in trackingDetails:
+                almEntityRestDetails = self.almEntityRestDetails("trace-matrix-report", project, self.baseUrl, ['trace-matrix-report'])
+                nextPageResponse = True
+                page_num = 1
+                page_size = 25
+                while nextPageResponse:
+                    restUrl = almEntityRestDetails.get('restUrl', None) + almEntityRestDetails.get('entityType', None) + "?page=" + str(page_num) + "&size=" + str(page_size)
+                    try:
+                        entityTypeResponse = self.getResponse(restUrl, 'GET', None, None, None, None, headers)                
+                        if entityTypeResponse.__len__() > 0:
+                            for res in entityTypeResponse:
+                                traceMatrixReport = res.get("requirements", {})
+                                for matrix in traceMatrixReport:
+                                    injectData= {}
+                                    if "testcases" in matrix:
+                                        injectData['modulesId'] = res.get('id', None)
+                                        injectData['projectId'] = int(project)
+                                        injectData['almType'] = "trace-matrix-report"
+                                        injectData['testcases'] = matrix.get("testcases", None)
+                                        injectData['linkedTestCases'] = matrix.get("linked-testcases", None)
+                                        injectData['id'] = matrix.get("id", None)
+                                        dataMatrix.append(injectData)
+                        else:
+                            nextPageResponse = False
+                    except Exception as urlError:
+                        nextPageResponse = False
+                        break
+                    page_num = page_num + 1
+        finally:
+            self.logout(token, self.baseUrl)
+        if len(dataMatrix) > 0:
+            self.publishToolsData(dataMatrix, metadata)
+    '''
+    def injectResponseData(self, data, responseTemplate, res, projectName, projectId, entityType):
+        injectData= {}
+        injectData['projectName'] = projectName
+        injectData['projectId'] = projectId
+        injectData['almType'] = entityType
+        if entityType == 'requirements':
+            if 'name' in res:
+                injectData['jiraKey'] = res.get('name', '').split(' ')[0]
+                combinedProperty = {}
+                if 'properties' in res:
+                    for property in res.get('properties', []):
+                        injectData[property.get('field_name')] = property.get('field_value')
+        if "children" in res:
+            for child in res.get('children', None):
+                data += self.parseResponse(responseTemplate, child, injectData)
+                self.injectResponseData(data, responseTemplate, child.get('children', None), projectName, projectId, entityType)
+        data += self.parseResponse(responseTemplate, res, injectData)
+    '''
 if __name__ == "__main__":
     QtestAgent()       
