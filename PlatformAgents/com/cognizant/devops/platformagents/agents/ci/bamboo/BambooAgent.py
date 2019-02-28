c73916e6201ccdf6e7f99aa7e7d1c66e167bfb96
DhrubajyotiDebnathcts <36297783+DhrubajyotiDebnathcts@users.noreply.github.com>
2019-02-28 10:29:43 +0530

    Bamboo agent upgrade (#200)
    * Adding all branch data fetch logic with correcting timestamp format and adding fields for 2.0 registration issue.
    
    * Adding all branch data fetch logic with correcting timestamp format and adding fields for 2.0 registration issue.

--- PlatformAgents/com/cognizant/devops/platformagents/agents/ci/bamboo/BambooAgent.py
+++ PlatformAgents/com/cognizant/devops/platformagents/agents/ci/bamboo/BambooAgent.py
@@ -25,46 +25,74 @@ class BambooAgent(BaseAgent):
         BaseUrl = self.config.get("baseUrl", None)
         UserID = self.config.get("userid", None)
         Passwd = self.config.get("passwd", None)
-        data = []
+        enableAllBranches = self.config.get("enableAllBranches", None)
         getCollectionUrl = BaseUrl + "rest/api/latest/"       
         buildsURL = getCollectionUrl +"plan.json"
         builds = self.getResponse(buildsURL,'GET', UserID, Passwd, None,None)        
         responseTemplate = self.getResponseTemplate()
         build_plan = builds["plans"]["plan"]
         for plan_Individual in build_plan:            
-            plan_Individual_Key =  plan_Individual.get('key')          
-            lastBuildNo = self.tracking.get(plan_Individual_Key,None) 
-            if lastBuildNo is None:
-                lastBuildNo = 0
-            plan_size = 25
-            start_index = 0
-            Build_running_Complete = True
-            plan_Individual_Url = getCollectionUrl + "result/" + plan_Individual_Key +".json?start-index=" + str(start_index) + "&max-result=25"
-            plan_Individual_collection = self.getResponse(plan_Individual_Url,'GET', UserID, Passwd, None,None)
-            self.tracking[plan_Individual_Key] = plan_Individual_collection["results"]["result"][0]["buildNumber"]
-            while Build_running_Complete:
-                for plan_Individual_result_length in range (plan_Individual_collection["results"]["size"]):
-                    plan_Individual_result = plan_Individual_collection["results"]["result"][plan_Individual_result_length]
-                    plan_Individual_result_key =  plan_Individual_result.get("key")            
-                if len(plan_Individual_collection["results"]["result"]) >0:
-                    if lastBuildNo < plan_Individual_collection["results"]["result"][0]["buildNumber"]:
-                        for plan_Individual_result_length in range(lastBuildNo,plan_Individual_collection["results"]["size"]):
-                            plan_Individual_result = plan_Individual_collection["results"]["result"][plan_Individual_result_length]
-                            plan_Individual_result_key =  plan_Individual_result.get("key")
-                            plan_Individual_result_url = getCollectionUrl + "result/" + plan_Individual_result_key +".json"
-                            plan_Individual_result_details = self.getResponse(plan_Individual_result_url,'GET', UserID, Passwd, None,None)
-                            injectData = {}
-                            data += self.parseResponse(responseTemplate, plan_Individual_result_details, injectData)
-                start_index = start_index + plan_size
+            plan_Individual_Key =  plan_Individual.get('key')  
+            alllBranches = []
+            jsonObj = {'branchKey': plan_Individual_Key, 'branchName':'master'}
+            alllBranches.append(jsonObj)
+            if enableAllBranches:
+                listAllBranchesUrl = getCollectionUrl + "plan/" + plan_Individual_Key +"/branch.json?max-result=1000"
+                branch_list_data = self.getResponse(listAllBranchesUrl,'GET', UserID, Passwd, None,None)
+                branch_list_data = branch_list_data.get('branches', {}).get('branch', [])
+                for branch in branch_list_data:
+                    branchName = branch.get('name')
+                    branchKey = branch.get('key')
+                    jsonObj = {'branchKey': branchKey, 'branchName':branchName}
+                    alllBranches.append(jsonObj)
+            for branch in alllBranches:
+                branch_name = branch.get('branchName')
+                branch_key = branch.get('branchKey')
+                plan_Individual_Key = branch_key
+                lastBuildNo = self.tracking.get(plan_Individual_Key,None) 
+                if lastBuildNo is None:
+                    lastBuildNo = 0
+                plan_size = 25
+                start_index = 0
+                Build_running_Complete = True
                 plan_Individual_Url = getCollectionUrl + "result/" + plan_Individual_Key +".json?start-index=" + str(start_index) + "&max-result=25"
                 plan_Individual_collection = self.getResponse(plan_Individual_Url,'GET', UserID, Passwd, None,None)
-                if plan_Individual_collection["results"]["size"] > 0:
-                    Build_running_Complete = True
-                else:
-                    Build_running_Complete = False
-            self.updateTrackingJson(self.tracking)
-        if len(data) >0:
-            self.publishToolsData(data)     
+                if len(plan_Individual_collection["results"]["result"]) == 0:
+                    self.tracking[plan_Individual_Key] = 0
+                    continue
+                self.tracking[plan_Individual_Key] = plan_Individual_collection["results"]["result"][0]["buildNumber"]
+                while Build_running_Complete:
+                    data = []
+                    for plan_Individual_result_length in range (plan_Individual_collection["results"]["size"]):
+                        plan_Individual_result = plan_Individual_collection["results"]["result"][plan_Individual_result_length]
+                        plan_Individual_result_key =  plan_Individual_result.get("key")            
+                    if len(plan_Individual_collection["results"]["result"]) >0:
+                        if lastBuildNo < plan_Individual_collection["results"]["result"][0]["buildNumber"]:
+                            for plan_Individual_result_length in range(lastBuildNo,plan_Individual_collection["results"]["size"]):
+                                injectData = {}
+                                plan_Individual_result = plan_Individual_collection["results"]["result"][plan_Individual_result_length]
+                                plan_Individual_result_key =  plan_Individual_result.get("key")
+                                plan_Individual_result_url = getCollectionUrl + "result/" + plan_Individual_result_key +".json"
+                                plan_Individual_result_details = self.getResponse(plan_Individual_result_url,'GET', UserID, Passwd, None,None)
+#                                 utc_time = datetime.strptime(plan_Individual_result_details["buildCompletedTime"].split(".")[0], "%Y-%m-%dT%H:%M:%S")
+#                                 epoch_time = (utc_time - datetime(1970, 1, 1)).total_seconds()
+#                                 injectData['buildCompletedTime'] = plan_Individual_result_details["buildCompletedTime"].split(".")[0]
+#                                 injectData['buildCompletedTimeEpoch'] = epoch_time
+#                                 injectData['inSightsTimeX'] = plan_Individual_result_details["buildCompletedTime"].split(".")[0]
+#                                 injectData['inSightsTime'] = epoch_time
+                                injectData['branchName'] = branch_name
+                                injectData['branchKey'] = branch_key
+                                data += self.parseResponse(responseTemplate, plan_Individual_result_details, injectData)
+                    start_index = start_index + plan_size
+                    plan_Individual_Url = getCollectionUrl + "result/" + plan_Individual_Key +".json?start-index=" + str(start_index) + "&max-result=25"
+                    plan_Individual_collection = self.getResponse(plan_Individual_Url,'GET', UserID, Passwd, None,None)
+                    if plan_Individual_collection["results"]["size"] > 0:
+                        Build_running_Complete = True
+                    else:
+                        Build_running_Complete = False
+                    if len(data) > 0:
+                        self.publishToolsData(data)
+                self.updateTrackingJson(self.tracking)     
 if __name__ == "__main__":
     BambooAgent()
 
