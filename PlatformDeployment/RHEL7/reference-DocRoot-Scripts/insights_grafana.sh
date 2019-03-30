3195b3a441c75f437078689ece333585859f558f
HariPrasath7 <42344667+HariPrasath7@users.noreply.github.com>
2019-03-30 10:20:42 +0530

    Grafana script changes (#207)
    * Update insights_grafana5.sh
    
    generalized folder for version in grafana
    
    * Deleted grafan4 scripts and added grafana5 scripts with a generis folder Key:162441019

--- PlatformDeployment/RHEL7/reference-DocRoot-Scripts/insights_grafana.sh
+++ PlatformDeployment/RHEL7/reference-DocRoot-Scripts/insights_grafana.sh
@@ -14,24 +14,25 @@
 # the License.
 #-------------------------------------------------------------------------------
 # Install customized Grafana V4.6.2
-echo "#################### Installing Grafana (running as BG process) with user creation ####################"
-sudo mkdir grafana-v4.6.2
-cd grafana-v4.6.2
-sudo wget https://platform.cogdevops.com/insights_install/installationScripts/latest/RHEL/grafana/grafana-4.6.2.linux-x64.tar.gz
-sudo tar -zxvf grafana-4.6.2.linux-x64.tar.gz
+echo "#################### Installing Grafana (running as BG process) ####################"
+sudo mkdir grafana
+cd grafana
+sudo wget https://platform.cogdevops.com/insights_install/installationScripts/latest/RHEL/grafana/latest/grafana.tar.gz
+sudo tar -zxvf grafana.tar.gz
+export GRAFANA_HOME=`pwd`
+sudo echo GRAFANA_HOME=`pwd` | sudo tee -a /etc/environment
+sudo echo "export" GRAFANA_HOME=`pwd` | sudo tee -a /etc/profile
+source /etc/environment
+source /etc/profile
 sudo wget https://platform.cogdevops.com/insights_install/installationScripts/latest/RHEL/grafana/ldap.toml
-sudo cp ldap.toml ./grafana-4.6.2/conf/ldap.toml
+sudo cp ldap.toml $GRAFANA_HOME/conf/ldap.toml
 sudo wget https://platform.cogdevops.com/insights_install/installationScripts/latest/RHEL/grafana/defaults.ini
-sudo cp defaults.ini ./grafana-4.6.2/conf/defaults.ini
-cd grafana-4.6.2
+sudo cp defaults.ini $GRAFANA_HOME/conf/defaults.ini
 sudo nohup ./bin/grafana-server &
 echo $! > grafana-pid.txt
 sleep 10
 curl -X POST -u admin:admin -H "Content-Type: application/json" -d '{"name":"PowerUser","email":"PowerUser@PowerUser.com","login":"PowerUser","password":"C0gnizant@1"}' http://localhost:3000/api/admin/users
 sleep 10
 cd ..
-export GRAFANA_HOME=`pwd`
-sudo echo GRAFANA_HOME=`pwd` | sudo tee -a /etc/environment
-sudo echo "export" GRAFANA_HOME=`pwd` | sudo tee -a /etc/profile
-source /etc/environment
-source /etc/profile
+
+
