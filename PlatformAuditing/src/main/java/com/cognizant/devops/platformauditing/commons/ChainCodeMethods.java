cce57a0753890ef341500a0dccc476dd8128aec6
Vijay Raghunath K M <757943@cognizant.com>
2019-05-21 08:21:26 +0000

    Revert "Merging enterprise repo to public for release mgmt"
    This reverts commit ac77992bf75fbad736c99f4e02e0b6466827abf1.

--- PlatformAuditing/src/main/java/com/cognizant/devops/platformauditing/commons/ChainCodeMethods.java
+++
@@ -1,9 +0,0 @@
-package com.cognizant.devops.platformauditing.commons;
-
-public interface ChainCodeMethods {
-	
-	public static final String INSTANTIATE = "Instantiate";
-	public static final String GETASSETSBYDATE = "getAssetsByDate";
-	public static final String GETASSETDETAILS = "GetAssetDetails";
-	public static final String GETASSETHISTORY = "GetAssetHistory";
-}
