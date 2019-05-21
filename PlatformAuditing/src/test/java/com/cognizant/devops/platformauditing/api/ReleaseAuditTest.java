06edb79bb7ca7d151d79a1e936275fd148056b1f
Vijay Raghunath K M <757943@cognizant.com>
2019-05-21 08:17:10 +0000

    Merging enterprise repo to public for release mgmt

---
+++ PlatformAuditing/src/test/java/com/cognizant/devops/platformauditing/api/ReleaseAuditTest.java
@@ -0,0 +1,14 @@
+package com.cognizant.devops.platformauditing.api;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ReleaseAuditTest {
+
+	@Test
+	public void test() {
+		assertTrue(true);
+	}
+
+}
