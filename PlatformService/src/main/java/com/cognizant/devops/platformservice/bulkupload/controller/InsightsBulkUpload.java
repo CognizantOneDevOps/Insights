1e0904a01686da750b10e1e5259d9975311c7825
Surbhi Gupta <surbhi.gupta@cognizant.com>
2019-06-14 18:04:22 +0530

    BulkUpload -API 166581052

--- PlatformService/src/main/java/com/cognizant/devops/platformservice/bulkupload/controller/InsightsBulkUpload.java
+++ PlatformService/src/main/java/com/cognizant/devops/platformservice/bulkupload/controller/InsightsBulkUpload.java
@@ -13,11 +13,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-
-import com.cognizant.devops.platformcommons.constants.ErrorMessage;
 import com.cognizant.devops.platformcommons.exception.InsightsCustomException;
 import com.cognizant.devops.platformservice.bulkupload.service.BulkUploadService;
-
 import com.cognizant.devops.platformservice.rest.neo4j.GraphDBService;
 import com.cognizant.devops.platformservice.rest.util.PlatformServiceUtil;
 import com.google.gson.JsonObject;
@@ -26,22 +23,17 @@ import com.google.gson.JsonObject;
 @RequestMapping("/admin/bulkupload")
 public class InsightsBulkUpload {
 	static Logger log = LogManager.getLogger(GraphDBService.class.getName());
-	private JsonObject asJsonObject;
-	@RequestMapping(value = "/uploadHierarchyDetails", headers = ("content-type=multipart/*"), method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_UTF8_VALUE, consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-	public @ResponseBody JsonObject uploadHierarchyDetails(@RequestParam("file") MultipartFile file,
-			@RequestParam String toolName) {
+	//private JsonObject asJsonObject;
+	
+	@RequestMapping(value = "/uploadToolData", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_UTF8_VALUE, consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+	public @ResponseBody JsonObject uploadToolData(@RequestParam("file") MultipartFile file, @RequestParam String toolName ) throws InsightsCustomException {
 		boolean status = false;
-		try {
-			
-				status = BulkUploadService.getInstance().createBusinessHierarchyMetaData(file);
-					
-			if (!status) {
-				return PlatformServiceUtil.buildFailureResponse(ErrorMessage.DB_INSERTION_FAILED);
-			}
-		} catch (InsightsCustomException e) {
-			return PlatformServiceUtil.buildFailureResponse(e.getMessage());
-		}
-
+		
+		status = BulkUploadService.getInstance().createBulkUploadMetaData(file,toolName);
+		log.error("HELLO");
+        log.error(file);
+        log.error(status);
+        log.error(PlatformServiceUtil.buildSuccessResponse());
 		return PlatformServiceUtil.buildSuccessResponse();
 
 	}
