532cf3e6089a51730a31041235d4d38805fd7dc5
Surbhi Gupta <surbhi.gupta@cognizant.com>
2019-07-04 12:36:10 +0530

    Code Formatting. 166581052

--- PlatformService/src/main/java/com/cognizant/devops/platformservice/bulkupload/service/IBulkUpload.java
+++ PlatformService/src/main/java/com/cognizant/devops/platformservice/bulkupload/service/IBulkUpload.java
@@ -1,5 +1,13 @@
 package com.cognizant.devops.platformservice.bulkupload.service;
 
-public interface IBulkUpload {
+import java.io.IOException;
 
+import org.springframework.web.multipart.MultipartFile;
+
+import com.cognizant.devops.platformcommons.exception.InsightsCustomException;
+
+public interface IBulkUpload {
+	public Object getToolDetailJson() throws InsightsCustomException;
+	public boolean uploadDataInDatabase(MultipartFile file, String toolName, String label)
+			throws InsightsCustomException, IOException;
 }
