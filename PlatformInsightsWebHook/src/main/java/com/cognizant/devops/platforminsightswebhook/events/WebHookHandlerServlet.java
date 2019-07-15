fdc3ed24cb578075071bf237e002b474298be383
gauravdeshmukh612 <gaurav.deshmukh2@cognizant.com>
2019-07-15 12:57:42 +0530

    166900398 Webhook handler

--- PlatformInsightsWebHook/src/main/java/com/cognizant/devops/platforminsightswebhook/events/WebHookHandlerServlet.java
+++ PlatformInsightsWebHook/src/main/java/com/cognizant/devops/platforminsightswebhook/events/WebHookHandlerServlet.java
@@ -1,4 +1,19 @@
-package com.congnizant.eventsubscriber.events;
+/*******************************************************************************
+ * Copyright 2017 Cognizant Technology Solutions
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy
+ * of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ ******************************************************************************/
+package com.cognizant.devops.platforminsightswebhook.events;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -12,44 +27,31 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
-
-import com.rabbitmq.client.Channel;
-import com.rabbitmq.client.Connection;
-import com.rabbitmq.client.ConnectionFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import com.cognizant.devops.platforminsightswebhook.config.WebHookMessagePublisher;
 
 
 /**
  * Servlet implementation class GitEvent
  */
-//@WebServlet("/webhookEvent")
+
 @WebServlet(urlPatterns = "/webhookEvent/*", loadOnStartup = 1)
-public class Event extends HttpServlet {
+public class WebHookHandlerServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private Long allrequestTime = 0L;
-	private ConnectionFactory factory;
-	private Channel channel;
-	private Connection connection;
-	private String routingKey = "WEBHOOK_EVENTDATA";
-	private String exchangeName = "iSight";
-	@Autowired
-	private Environment env;
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public Event() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+	private static Logger LOG = LogManager.getLogger(WebHookHandlerServlet.class);
+
+	//@Autowired
+	WebHookMessagePublisher webhookmessagepublisher = new WebHookMessagePublisher();
     
     @Override
 	public void init() throws ServletException {
 		try {
-			initilizeMq();
-		} catch (TimeoutException e) {
-			// TODO Auto-generated catch block
+			LOG.debug(" In server init .... initilizeMq ");
+			webhookmessagepublisher.initilizeMq();
+		} catch (Exception e) {
+			LOG.error("Error while initilize mq " + e.getMessage());
 			e.printStackTrace();
 		}
     }
@@ -61,24 +63,24 @@ public class Event extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		System.out.println(" In only doGet not post  Git Git Git ");
-		
+		LOG.debug(" In only doGet not post Git ");
+
 		try {
 			
 			long millis = System.currentTimeMillis();
 			String res = getBody(request);
-			System.out.println("Current time in millis: after getBody  "+(System.currentTimeMillis()-millis));
+			LOG.debug("Current time in millis: after getBody  " + (System.currentTimeMillis() - millis));
 			millis = System.currentTimeMillis();
-			//System.out.println(res);
-			//System.out.println(request.getContentType());
+			//LOG.debug(res);
+			//LOG.debug(request.getContentType());
 			
 			System.out.print(" before publish " + (System.currentTimeMillis() - millis));
 
-			publishEventAction(res.getBytes());
+			webhookmessagepublisher.publishEventAction(res.getBytes());
 			
 			long requestTime = (System.currentTimeMillis() - millis);
 			allrequestTime = allrequestTime + requestTime;
-			System.out.println("Current time in millis: " + requestTime + "  allrequestTime  " + allrequestTime);
+			LOG.debug("Current time in millis: " + requestTime + "  allrequestTime  " + allrequestTime);
 
 			//doGet(request, response);
 			
@@ -96,18 +98,18 @@ public class Event extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		try {
-			//System.out.println("In do post ");
+			//LOG.debug("In do post ");
 			Enumeration<String> parameterNames = request.getParameterNames();
 
 			while (parameterNames.hasMoreElements()) {
 
 				String paramName = parameterNames.nextElement();
-				//System.out.println(" paramName " + paramName);
+				//LOG.debug(" paramName " + paramName);
 
 				String[] paramValues = request.getParameterValues(paramName);
 				for (int i = 0; i < paramValues.length; i++) {
 					String paramValue = paramValues[i];
-					//System.out.println(" paramValues " + paramValue);
+					//LOG.debug(" paramValues " + paramValue);
 				}
 
 			}
@@ -116,16 +118,16 @@ public class Event extends HttpServlet {
 		String res = getBody(request);
 			//System.out.print("Current time in millis: after getBody  "+(System.currentTimeMillis()-millis));
 		millis = System.currentTimeMillis();
-		//System.out.println(res);
-		//System.out.println(request.getContentType());
+			//LOG.debug(res);
+			//LOG.debug(request.getContentType());
 		
 			System.out.print(" before publish " + (System.currentTimeMillis() - millis));
 
-		publishEventAction(res.getBytes());
+			webhookmessagepublisher.publishEventAction(res.getBytes());//webHookMessagePublisher
 		
 			long requestTime = (System.currentTimeMillis() - millis);
 			allrequestTime = allrequestTime + requestTime;
-			System.out.println(" Current time in millis: " + requestTime + "  allrequestTime  " + allrequestTime);
+			LOG.debug(" Current time in millis: " + requestTime + "  allrequestTime  " + allrequestTime);
 		//doGet(request, response);
 		
 		} catch (TimeoutException e) {
@@ -169,64 +171,9 @@ public class Event extends HttpServlet {
 	   return body;
 	}
 	
-	/*
-	 * public void pushDataToRabbitMq(String res) { ConnectionFactory factory = new
-	 * ConnectionFactory(); factory.setHost("localhost"); try (Connection connection
-	 * = factory.newConnection(); Channel channel = connection.createChannel()) {
-	 * channel.queueDeclare(QUEUE_NAME, false, false, false, null); String message =
-	 * "Hello World!"; channel.basicPublish("", QUEUE_NAME, null,
-	 * message.getBytes("UTF-8")); System.out.println(" [x] Sent '" + message +
-	 * "'"); } }
-	 */
-	
-	
-	private void publishEventAction(byte[] data)
-			throws TimeoutException, IOException {
-		
-		if (channel != null) {
-			channel.basicPublish(exchangeName, routingKey, null, data);
-		}
-
-
-	}
-
-	private void initilizeMq() throws TimeoutException {
-
-
-		try {
-			//String hostPath = env.getProperty("mq.host");
-			//System.out.println("  hostPath  " + hostPath);
-			factory = new ConnectionFactory();
-			factory.setHost("localhost");
-			factory.setUsername("iSight");
-			factory.setPassword("iSight");
-			connection = factory.newConnection();
-			channel = connection.createChannel();
-			channel.exchangeDeclare(exchangeName, "topic", true);
-			channel.queueDeclare(routingKey, true, false, false, null);
-			channel.queueBind(routingKey, exchangeName, routingKey);
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-	}
-
 	@Override
 	public void destroy() {
-
-		try {
-			if (channel != null && connection != null) {
-				channel.close();
-				connection.close();
-			}
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (TimeoutException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		webhookmessagepublisher.releaseMqConnetion();
 	}
 
 }
