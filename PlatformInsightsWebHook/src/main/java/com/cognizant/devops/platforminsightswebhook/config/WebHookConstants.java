69fac506ed7ed6c22d92be7d3ca9fb5e17ed330d
gauravdeshmukh612 <gaurav.deshmukh2@cognizant.com>
2019-07-16 23:31:50 +0530

    166900398 Added some more validation and constant file

---
+++ PlatformInsightsWebHook/src/main/java/com/cognizant/devops/platforminsightswebhook/config/WebHookConstants.java
@@ -0,0 +1,7 @@
+package com.cognizant.devops.platforminsightswebhook.config;
+
+public interface WebHookConstants {
+
+	public static final String REQUEST_PARAM_KEY_WEBHOOKNAME = "webHookName";
+	public static final String EXCHANGE_TYPE = "topic";
+}
