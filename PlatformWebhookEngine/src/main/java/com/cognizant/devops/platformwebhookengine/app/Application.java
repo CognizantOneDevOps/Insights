e65ceaeddaa02c6e061ce6b944f764e9243a4f5f
Surbhi Gupta <surbhi.gupta@cognizant.com>
2019-07-25 14:53:14 +0530

    166900271 :- WebhookEngine Code.

---
+++ PlatformWebhookEngine/src/main/java/com/cognizant/devops/platformwebhookengine/app/Application.java
@@ -0,0 +1,83 @@
+/*******************************************************************************
+ * Copyright 2017 Cognizant Technology Solutions
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License.  You may obtain a copy
+ * of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ ******************************************************************************/
+
+package com.cognizant.devops.platformwebhookengine.app;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.quartz.JobBuilder;
+import org.quartz.JobDetail;
+import org.quartz.Scheduler;
+import org.quartz.SchedulerException;
+import org.quartz.SimpleScheduleBuilder;
+import org.quartz.Trigger;
+import org.quartz.TriggerBuilder;
+import org.quartz.impl.StdSchedulerFactory;
+
+import com.cognizant.devops.platformcommons.config.ApplicationConfigCache;
+import com.cognizant.devops.platformcommons.config.ApplicationConfigProvider;
+
+
+import com.cognizant.devops.platformwebhookengine.modules.aggregator.EngineAggregatorModule;
+
+public class Application {
+private static Logger log = LogManager.getLogger(Application.class.getName());
+	
+	private static int defaultInterval = 600;
+	private Application(){
+		
+	}
+
+	public static void main(String[] args) throws Exception{
+		if(args.length > 0){
+			defaultInterval = Integer.valueOf(args[0]);
+		}
+		try {
+			// Load insight configuration
+			ApplicationConfigCache.loadConfigCache();
+			
+			ApplicationConfigProvider.performSystemCheck();
+			
+			// Subscribe for desired events.
+			
+			JobDetail aggrgatorJob = JobBuilder.newJob(EngineAggregatorModule.class)
+					.withIdentity("EngineAggregatorModule", "iSight").build();
+           
+			Trigger aggregatorTrigger = TriggerBuilder.newTrigger()
+					.withIdentity("EngineAggregatorModuleTrigger", "iSight")
+					.startNow()
+					.withSchedule(SimpleScheduleBuilder.simpleSchedule()
+							.withIntervalInSeconds(defaultInterval)
+							.repeatForever())
+					.build();
+			
+			
+			Scheduler scheduler;
+			
+			scheduler = new StdSchedulerFactory().getScheduler();
+			scheduler.start();
+		    scheduler.scheduleJob(aggrgatorJob, aggregatorTrigger);
+			//EngineStatusLogger.getInstance().createEngineStatusNode("Platform Engine Service Started ",PlatformServiceConstants.SUCCESS);
+	
+		} catch (SchedulerException e) {
+		//EngineStatusLogger.getInstance().createEngineStatusNode("Platform Engine Service not running due to Scheduler Exception "+e.getMessage(),PlatformServiceConstants.FAILURE);
+		log.error(e);
+	}catch (Exception e) {
+		//EngineStatusLogger.getInstance().createEngineStatusNode("Platform Engine Service not running "+e.getMessage(),PlatformServiceConstants.FAILURE);
+		log.error(e);
+	}
+}
+
+	}
---
+++ PlatformWebhookEngine/src/main/java/com/cognizant/devops/platformwebhookengine/message/subscriber/WebHookDataSubscriber.java
@@ -0,0 +1,260 @@
+/*******************************************************************************
+ * Copyright 2017 Cognizant Technology Solutions
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License.  You may obtain a copy
+ * of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ ******************************************************************************/
+package com.cognizant.devops.platformwebhookengine.message.subscriber;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.cognizant.devops.platformcommons.constants.MessageConstants;
+import com.cognizant.devops.platformcommons.dal.neo4j.Neo4jDBHandler;
+//import com.cognizant.devops.platformengine.message.core.EngineStatusLogger;
+import com.cognizant.devops.platformwebhookengine.message.factory.EngineSubscriberResponseHandler;
+import com.cognizant.devops.platformwebhookengine.modules.aggregator.WebhookMappingData;
+import com.github.wnameless.json.flattener.JsonFlattener;
+import com.github.wnameless.json.unflattener.JsonUnflattener;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.rabbitmq.client.AMQP.BasicProperties;
+import com.rabbitmq.client.Envelope;
+
+public class WebHookDataSubscriber extends EngineSubscriberResponseHandler {
+
+	private static Logger log = LogManager.getLogger(WebHookDataSubscriber.class.getName());
+	String responseTemplate;
+	JsonElement responseTemplateJson;
+
+	public WebHookDataSubscriber(String routingKey, String responseTemplate) throws Exception {
+		super(routingKey);
+
+		this.responseTemplate = responseTemplate;
+
+	}
+
+	/*
+	 * private boolean dataUpdateSupported; private String uniqueKey; private String
+	 * category; private String toolName;
+	 */
+	List<WebhookMappingData> webhookMappingList = new ArrayList<WebhookMappingData>(0);
+
+	public void handleDelivery(String consumerTag, Envelope envelope, BasicProperties properties, byte[] body)
+			throws IOException {
+		String keyMq = "";
+		String keyRT = "";
+		String keyMqInitial;
+		String keyRTInitial;
+		String removeString = "";
+		Neo4jDBHandler dbHandler = new Neo4jDBHandler();
+		String message = new String(body, MessageConstants.MESSAGE_ENCODING);
+		String routingKey = envelope.getRoutingKey();
+		// log.debug(responseTemplate);
+
+		routingKey = routingKey.replace("_", ".");
+		// log.debug(consumerTag+" [x] Received '" + routingKey + "':'" + message +"'");
+
+		JsonParser parser = new JsonParser();
+		JsonElement json = (JsonElement) parser.parse(message);
+		// log.debug(json);
+		JsonElement responseTemplateJson = (JsonElement) parser.parse(responseTemplate);
+
+		// String flattenedJson =
+		// JsonFlattener.flatten(responseTemplateJson.toString());
+		// log.debug("\n=====Simple Flatten===== \n" + flattenedJson);
+		Map<String, Object> responseTemplateflattenedJsonMap = JsonFlattener
+				.flattenAsMap(responseTemplateJson.toString());
+		// log.debug("\n=====Simple Flatten===== \n" +
+		// responseTemplateflattenedJsonMap);
+
+		Map<String, Object> rabbitMqflattenedJsonMap = JsonFlattener.flattenAsMap(json.toString());
+		System.out.println("Size of the rabbit mq json.." + rabbitMqflattenedJsonMap.size());
+		Map<String, Object> finalJson = new HashMap<String, Object>();
+		// log.debug("\n=====Simple Flatten===== \n" + rabbitMqflattenedJsonMap);
+		char b1;
+		char b2;
+		Boolean hault = false;
+		// rabbitMqflattenedJsonMap.forEach((k, v) -> keyMq = k);
+		for (Map.Entry<String, Object> entry : rabbitMqflattenedJsonMap.entrySet()) {
+			System.out.println("Key = " + entry.getKey() + ", Value = " + entry.getValue());
+			keyMqInitial = entry.getKey();
+			int l1 = keyMqInitial.length();
+			// System.out.println(l1);
+			for (int i = 0; i < l1; i++) {
+				b1 = keyMqInitial.charAt(i);
+				keyMq = keyMqInitial;
+				System.out.print(b1);
+				if (b1 == '[') {
+					hault = true;
+				} else if (b1 == ']') {
+					hault = false;
+
+					removeString = removeString.substring(1);
+					log.error(removeString);
+					keyMq = keyMqInitial.replaceAll(removeString, "");
+					// System.out.println("keyMq"+keyMq);
+					removeString = "";
+				}
+
+				if (hault) {
+					removeString = removeString + b1;
+				}
+
+			}
+
+			for (Map.Entry<String, Object> insideEntry : responseTemplateflattenedJsonMap.entrySet()) {
+				keyRTInitial = insideEntry.getKey();
+				System.out.println(keyRTInitial);
+				int l2 = keyRTInitial.length();
+				log.error("Length of the key of Response Template" + l2);
+				for (int i = 0; i < l2; i++) {
+					b1 = keyRTInitial.charAt(i);
+					keyRT = keyRTInitial;
+
+					if (b1 == '[') {
+						hault = true;
+					} else if (b1 == ']') {
+						hault = false;
+
+						removeString = removeString.substring(1);
+						keyRT = keyRTInitial.replaceAll(removeString, "");
+						System.out.println("Final string after the removal of index in response template " + keyRT);
+						removeString = "";
+					}
+
+					if (hault) {
+						removeString = removeString + b1;
+					}
+
+				}
+				Boolean testResult;
+				System.out.println("bEFORE Compare...keyRT" + keyRT);
+				System.out.println("bEFORE Compare...keyMQ" + keyMq);
+				testResult = keyMq.equals(keyRT);
+				// log.error(testResult);
+
+				if (testResult) {
+					finalJson.put(keyRTInitial, entry.getValue());
+					System.out.println(finalJson);
+				}
+
+			}
+		}
+		// sGsonBuilder gsonMapBuilder = new GsonBuilder();
+		/*
+		 * Gson gsonObject = gsonMapBuilder.create(); String JSONObject =
+		 * gsonObject.toJson(finalJson);
+		 * 
+		 * System.out.println("First method"+JSONObject);
+		 */
+		Gson prettyGson = new GsonBuilder().setPrettyPrinting().create();
+		String prettyJson = prettyGson.toJson(finalJson);
+		System.out.println("second method" + prettyJson);
+		String nestedJson = JsonUnflattener.unflatten(prettyJson);
+		System.out.println("\n=====Unflatten it back to original JSON===== \n" + nestedJson);
+
+		/*
+		 * String maptostring= finalJson.toString(); String nestedJson =
+		 * JsonUnflattener.unflatten(maptostring);
+		 * 
+		 * JsonObject finaljson = (JsonObject) parser.parse(nestedJson);
+		 * System.out.println("\n=====Unflatten it back to original JSON===== \n" +
+		 * nestedJson);
+		 */
+
+	}
+
+	// processJson(json);
+	// processJson(responseTemplateJson);
+
+	private void processJson(JsonElement jsonElement) {
+		// {"results":[{"columns":["n"],"data":[{"row":[{"name":"Test
+		// me"}]},{"row":[{"name":"Test me"}]}]}],"errors":[]}
+		if (jsonElement.isJsonNull()) {
+			log.debug("Null value " + jsonElement);
+		} else if (jsonElement.isJsonArray()) {
+			JsonArray jsonArray = jsonElement.getAsJsonArray();
+			log.error("Json Array found " + jsonArray.size());
+			if (jsonArray.size() > 0) {
+				for (JsonElement jsonArrayElement : jsonArray) {
+					if (jsonArrayElement.isJsonObject()) {
+						processJson(jsonArrayElement);
+					} else {
+						log.debug("Elemnet.." + jsonArrayElement);
+					}
+				}
+			} else {
+				log.debug("Null value" + jsonArray);
+			}
+		} else {
+			log.error(jsonElement);
+			JsonObject jsonObject = jsonElement.getAsJsonObject();
+			log.debug(jsonObject);
+			if (!jsonObject.isJsonNull()) {
+
+				for (Map.Entry<String, JsonElement> entry : jsonObject.entrySet()) {
+
+					if (entry.getValue().isJsonNull()) {
+						log.error("Null Value Found" + entry.getKey());
+					} else if (entry.getValue().isJsonArray()) {
+						processJson(entry.getValue());
+						log.debug("returened from rabbit mq function");
+
+					} else if (!entry.getValue().isJsonPrimitive()) {
+						parseJsonPrimitive(entry);
+						log.debug("Comeback from Json Promitive");
+
+					} else {
+
+						log.debug("  Key..." + entry.getKey() + "   Value..." + entry.getValue());
+					}
+
+				}
+			}
+		}
+	}
+
+	private void parseJsonPrimitive(Map.Entry<String, JsonElement> entry) {
+		log.debug("Entry..." + entry);
+
+		if (entry.getValue().isJsonArray()) {
+			processJson(entry.getValue());
+		} else if (!entry.getValue().isJsonNull()) {
+			log.error("Entered the not null conditionss");
+			JsonObject jsonObjectInternal = entry.getValue().getAsJsonObject();
+			for (Map.Entry<String, JsonElement> entryAgain : jsonObjectInternal.entrySet()) {
+				if (entry.getValue().isJsonArray()) {
+					processJson(entry.getValue());
+				} else if (!entryAgain.getValue().isJsonNull() && !entryAgain.getValue().isJsonPrimitive()) {
+					parseJsonPrimitive(entryAgain);
+				} else {
+					log.debug("Key internal ..." + entryAgain.getKey() + " Value..." + entryAgain.getValue());
+				}
+			}
+		}
+
+		else {
+			log.debug(" value of Key internal is null  ..." + entry.getKey());
+		}
+	}
+}
