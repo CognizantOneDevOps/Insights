b7824059eec4a996979385878c6ed8e16ded463c
sinswa28 <swati.banerjee@cognizant.com>
2019-08-13 19:22:21 +0530

    #167722780: Health Check Changes for Failed Updates

--- PlatformEngine/src/main/java/com/cognizant/devops/platformengine/message/subscriber/AgentHealthSubscriber.java
+++ PlatformEngine/src/main/java/com/cognizant/devops/platformengine/message/subscriber/AgentHealthSubscriber.java
@@ -50,6 +50,7 @@ public class AgentHealthSubscriber extends EngineSubscriberResponseHandler{
 		log.debug(consumerTag+" [x] Received '" + routingKey + "':'" + message + "'");
 		List<String> labels = Arrays.asList(routingKey.split(MessageConstants.ROUTING_KEY_SEPERATOR));
 		List<JsonObject> dataList = new ArrayList<JsonObject>();
+		List<JsonObject> failedDataList = new ArrayList<JsonObject>();
 		JsonElement json = new JsonParser().parse(message);
 		String agentId="";
 		String toolName="";
@@ -87,6 +88,7 @@ public class AgentHealthSubscriber extends EngineSubscriberResponseHandler{
 						healthStatus =jsonObject.get("status").getAsString();
 						if(healthStatus.equalsIgnoreCase("failure")) {
 							isFailure=true;
+							failedDataList.add(jsonObject);
 						}
 					}
 					dataList.add(jsonObject);
@@ -94,97 +96,75 @@ public class AgentHealthSubscriber extends EngineSubscriberResponseHandler{
 			}
 			try {
 				String healthLabels = ":LATEST:"+routingKey.replace(".", ":");
-				String healthQuery;
-				// For Sequential/successive agent health publishing where agentId is not null
-				if(!agentId.equalsIgnoreCase("")) {
-					healthQuery= "Match";
-					healthQuery = healthQuery + " (old"+healthLabels+")"; // {} 
-					healthQuery = healthQuery + " where old.agentId='"+agentId+"' or old.agentId is null";
-					healthQuery = healthQuery + " OPTIONAL MATCH (old) <-[:UPDATED_TO*10]-(purge)  where old.agentId='"+agentId+"'";
-					healthQuery = healthQuery + " CREATE (new"+healthLabels+" {props}) ";
-					healthQuery = healthQuery + " MERGE  (new)<-[r:UPDATED_TO]-(old)";
-					healthQuery = healthQuery + " REMOVE old:LATEST";
-					healthQuery = healthQuery + " detach delete purge ";
-					healthQuery = healthQuery + " return old,new";
-			
-				}
-				// For first time agent health publishing when agentId is null
-				else {
-					healthQuery = "Match (old"+healthLabels+")";
-					healthQuery = healthQuery + " OPTIONAL MATCH (old) <-[:UPDATED_TO*10]-(purge) ";
-					healthQuery = healthQuery + " CREATE (new"+healthLabels+" {props})";
-					healthQuery = healthQuery + " MERGE  (new)<-[r:UPDATED_TO]-(old)";
-					healthQuery = healthQuery + " REMOVE old:LATEST";
-					healthQuery = healthQuery + " detach delete purge ";
-					healthQuery = healthQuery + " return old,new";
-				}			
-				
-				JsonObject graphResponse = dbHandler.executeQueryWithData(healthQuery, dataList);
-				if(graphResponse.get("response").getAsJsonObject().get("results").getAsJsonArray().get(0).getAsJsonObject().get("data")
-						.getAsJsonArray().size() == 0) {
-					//log.debug("arg0 === data not present ====== " );
-					healthQuery="";
-					healthQuery = healthQuery + " CREATE (new"+healthLabels+" {props})";
-					JsonObject graphResponse1 = dbHandler.executeQueryWithData(healthQuery, dataList);
-					//log.debug("arg1 ====== "+graphResponse1.get("response"));
-				}
-				if(graphResponse.get("response").getAsJsonObject().get("errors").getAsJsonArray().size() > 0){
-					log.error("Unable to insert health nodes for routing key: "+routingKey+", error occured: "+graphResponse);
-					log.error(dataList);
-					EngineStatusLogger.getInstance().createEngineStatusNode("Unable to insert health nodes for routing key: "+routingKey,PlatformServiceConstants.FAILURE);
-				}
+				createHealthNodes(dbHandler, routingKey, dataList, agentId, healthLabels,10,false);
 				
 				if(isFailure) {
 					String failureLabels = routingKey.replace(".", ":");
 					failureLabels = failureLabels.replace("HEALTH", "HEALTH_FAILURE");
-					String healthFailureLabels = ":LATEST:"+ failureLabels ;
-					System.out.println(" #### healthFailureLabels ==== " + healthFailureLabels);
-					String healthFailureQuery;
-					// For Sequential/successive agent health publishing where agentId is not null
-					if(!agentId.equalsIgnoreCase("")) {
-						healthFailureQuery= "Match";
-						healthFailureQuery = healthFailureQuery + " (old"+ healthFailureLabels +")";  
-						healthFailureQuery = healthFailureQuery + " where old.agentId='"+agentId+"' or old.agentId is null";
-						healthFailureQuery = healthFailureQuery + " OPTIONAL MATCH (old) <-[:UPDATED_TO*20]-(purge)  where old.agentId='"+agentId+"'";
-						healthFailureQuery = healthFailureQuery + " CREATE (new"+ healthFailureLabels +" {props}) ";
-						healthFailureQuery = healthFailureQuery + " MERGE  (new)<-[r:UPDATED_TO]-(old)";
-						healthFailureQuery = healthFailureQuery + " REMOVE old:LATEST";
-						healthFailureQuery = healthFailureQuery + " detach delete purge ";
-						healthFailureQuery = healthFailureQuery + " return old,new";
-				
-					}
-					// For first time agent health publishing when agentId is null
-					else {
-						healthFailureQuery = "Match (old"+ healthFailureLabels +")";
-						healthFailureQuery = healthFailureQuery + " OPTIONAL MATCH (old) <-[:UPDATED_TO*20]-(purge) ";
-						healthFailureQuery = healthFailureQuery + " CREATE (new"+ healthFailureLabels +" {props})";
-						healthFailureQuery = healthFailureQuery + " MERGE  (new)<-[r:UPDATED_TO]-(old)";
-						healthFailureQuery = healthFailureQuery + " REMOVE old:LATEST";
-						healthFailureQuery = healthFailureQuery + " detach delete purge ";
-						healthFailureQuery = healthFailureQuery + " return old,new";
-					}
-					
-					log.debug("======= healthFailureQuery >>>> " + healthFailureQuery );
-					JsonObject queryResponse = dbHandler.executeQueryWithData(healthFailureQuery, dataList);
-					if(queryResponse.get("response").getAsJsonObject().get("results").getAsJsonArray().get(0).getAsJsonObject().get("data")
-							.getAsJsonArray().size() == 0) {
-						log.debug("arg0 === data not present ====== " );
-						healthFailureQuery="";
-						healthFailureQuery = healthFailureQuery + " CREATE (new"+healthFailureLabels+" {props})";
-						JsonObject queryResponse1 = dbHandler.executeQueryWithData(healthFailureQuery, dataList);
-						log.debug("arg1 ====== "+queryResponse1.get("response"));
-					}
-					if(queryResponse.get("response").getAsJsonObject().get("errors").getAsJsonArray().size() > 0){
-						log.error("Unable to insert health nodes for routing key: "+routingKey+", error occured: " + queryResponse);
-						log.error(dataList);
-						EngineStatusLogger.getInstance().createEngineStatusNode("Unable to insert health nodes for routing key: "+routingKey,PlatformServiceConstants.FAILURE);
-					}
-				}		
-				
+					String healthFailureLabels = ":"+ failureLabels ;
+					createHealthNodes(dbHandler, routingKey, failedDataList, agentId, healthFailureLabels,20,true);					
+				}				
 				getChannel().basicAck(envelope.getDeliveryTag(), false);
 			} catch (GraphDBException e) {
 				log.error(e);
 			}
+		}
+	}
+
+	/**
+	 * @param dbHandler
+	 * @param routingKey
+	 * @param dataList
+	 * @param agentId
+	 * @param nodeLabels
+	 * @throws GraphDBException
+	 */
+	private void createHealthNodes(Neo4jDBHandler dbHandler, String routingKey, List<JsonObject> dataList,
+			String agentId, String nodeLabels, int nodeCount,boolean isHealthFailureNode) throws GraphDBException {
+		String healthQuery;
+		// For Sequential/successive agent health publishing where agentId is not null
+		if(!agentId.equalsIgnoreCase("")) {
+			healthQuery= "Match";
+			healthQuery = healthQuery + " (old"+nodeLabels+")"; 
+			healthQuery = healthQuery + " where old.agentId='"+agentId+"' or old.agentId is null";
+			healthQuery = healthQuery + " OPTIONAL MATCH (old) <-[:UPDATED_TO*"+ nodeCount +"]-(purge)  where old.agentId='"+agentId+"'";
+			healthQuery = healthQuery + " CREATE (new"+ nodeLabels +" {props}) ";
+			healthQuery = healthQuery + " MERGE  (new)<-[r:UPDATED_TO]-(old)";
+			// HEALTH_FAILURE Node won't have LATEST label, this check is to remove Latest label only from HEALTH node
+			if(!isHealthFailureNode) {
+				healthQuery = healthQuery + " REMOVE old:LATEST";				
+			}
+			healthQuery = healthQuery + " detach delete purge ";
+			healthQuery = healthQuery + " return old,new";
+
+		}
+		// For first time agent health publishing when agentId is null
+		else {
+			healthQuery = "Match (old"+nodeLabels+")";
+			healthQuery = healthQuery + " OPTIONAL MATCH (old) <-[:UPDATED_TO*"+ nodeCount +"]-(purge) ";
+			healthQuery = healthQuery + " CREATE (new" + nodeLabels + " {props})";
+			healthQuery = healthQuery + " MERGE  (new)<-[r:UPDATED_TO]-(old)";
+			// HEALTH_FAILURE Node won't have LATEST label, this check is to remove Latest label only from HEALTH node
+			if(!isHealthFailureNode) {
+				healthQuery = healthQuery + " REMOVE old:LATEST";				
+			}
+			healthQuery = healthQuery + " detach delete purge ";
+			healthQuery = healthQuery + " return old,new";
+		}			
+		
+		JsonObject graphResponse = dbHandler.executeQueryWithData(healthQuery, dataList);
+		if(graphResponse.get("response").getAsJsonObject().get("results").getAsJsonArray().get(0).getAsJsonObject().get("data")
+				.getAsJsonArray().size() == 0) {
+			//log.debug("arg0 === data not present ====== " );
+			healthQuery="";
+			healthQuery = healthQuery + " CREATE (new"+nodeLabels+" {props})";
+			JsonObject graphResponse1 = dbHandler.executeQueryWithData(healthQuery, dataList);
+			//log.debug("arg1 ====== "+graphResponse1.get("response"));
+		}
+		if(graphResponse.get("response").getAsJsonObject().get("errors").getAsJsonArray().size() > 0){
+			log.error("Unable to insert health nodes for routing key: "+routingKey+", error occured: "+graphResponse);
+			log.error(dataList);
+			EngineStatusLogger.getInstance().createEngineStatusNode("Unable to insert health nodes for routing key: "+routingKey,PlatformServiceConstants.FAILURE);
 		}
 	}	
 }
--- PlatformUI3/src/app/com/cognizant/devops/platformui/common.services/rest-apiurl.service.ts
+++ PlatformUI3/src/app/com/cognizant/devops/platformui/common.services/rest-apiurl.service.ts
@@ -34,6 +34,7 @@ export class RestAPIurlService implements IRestAPIUrlService {
         this.addEndPoint("HEALTH_TOOL", '/PlatformService/admin/health/detailHealth');
         this.addEndPoint("INSIGHTS_COMP_STATUS", '/PlatformService/admin/health/globalHealth');
         this.addEndPoint("AGENT_COMP_STATUS", '/PlatformService/admin/health/globalAgentsHealth');
+        this.addEndPoint("AGENTS_FAILURE_DETAILS", '/PlatformService/admin/health/getAgentFailureDetails');
 
         this.addEndPoint("LOGOUT", '/PlatformService/user/logout');
         this.addEndPoint("GRAPANA_CURRENT_ROLE_ORG", '/PlatformService/user/getCurrentOrgAndRole');
