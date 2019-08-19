353401750bb69d391e0d86bdb8c946d7e2cce8aa
pooja Gupta <pooja.gupta9@cognizant.com>
2019-08-19 13:02:23 +0530

    #167861951 "Bulkupload Test Cases"

--- PlatformService/src/test/java/com/cognizant/devops/platformservice/test/bulkUpload/BulkUploadTestData.java
+++ PlatformService/src/test/java/com/cognizant/devops/platformservice/test/bulkUpload/BulkUploadTestData.java
@@ -1,14 +1,17 @@
 package com.cognizant.devops.platformservice.test.bulkUpload;
 
-import org.springframework.web.multipart.MultipartFile;
+import java.io.File;
 
 public class BulkUploadTestData {
-	
-	String toolJson ="[{ \"toolName\":\"GIT\",\"label\":\"SCM:GIT:DATA\"}]";
-	
+
+	String toolJson = "[{ \"toolName\":\"GIT\",\"label\":\"SCM:GIT:DATA\"}]";
+
+	File file = new File("GIT.csv");
 	String toolName = "GIT";
-	String label  = "SCM:GIT:DATA";
-	
-	                	 
-	                	  
+	String label = "SCM:GIT:DATA";
+
+	File fileNull = new File("JENKINS.csv");
+	String toolNameNull = null;
+	String labelNull = null;
+
 }
