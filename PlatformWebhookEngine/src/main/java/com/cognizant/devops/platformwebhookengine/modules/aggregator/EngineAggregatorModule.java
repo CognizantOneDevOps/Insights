2abf60b0b5be050aa1c7edc97f7be116df352b8c
gauravdeshmukh612 <gaurav.deshmukh2@cognizant.com>
2019-08-20 10:39:23 +0530

    167862725 added acknowledge status in webhook engine

--- PlatformWebhookEngine/src/main/java/com/cognizant/devops/platformwebhookengine/modules/aggregator/EngineAggregatorModule.java
+++ PlatformWebhookEngine/src/main/java/com/cognizant/devops/platformwebhookengine/modules/aggregator/EngineAggregatorModule.java
@@ -2,20 +2,19 @@
  * Copyright 2017 Cognizant Technology Solutions
  * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License.  You may obtain a copy
+ * use this file except in compliance with the License. You may obtain a copy
  * of the License at
  * 
- *   http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
  * License for the specific language governing permissions and limitations under
  * the License.
  ******************************************************************************/
 package com.cognizant.devops.platformwebhookengine.modules.aggregator;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -27,68 +26,65 @@ import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 
 import com.cognizant.devops.platformcommons.config.ApplicationConfigProvider;
+import com.cognizant.devops.platformcommons.constants.PlatformServiceConstants;
 import com.cognizant.devops.platformcommons.dal.neo4j.Neo4jDBHandler;
 import com.cognizant.devops.platformdal.webhookConfig.WebHookConfig;
 import com.cognizant.devops.platformdal.webhookConfig.WebHookConfigDAL;
+import com.cognizant.devops.platformwebhookengine.message.core.WebhookEngineStatusLogger;
 import com.cognizant.devops.platformwebhookengine.message.factory.EngineSubscriberResponseHandler;
 import com.cognizant.devops.platformwebhookengine.message.subscriber.WebHookDataSubscriber;
 
 public class EngineAggregatorModule implements Job {
-    private static Logger log = LogManager.getLogger(EngineAggregatorModule.class.getName());
-    private static Map < String, EngineSubscriberResponseHandler > registry = new HashMap < String, EngineSubscriberResponseHandler > ();
+	private static Logger log = LogManager.getLogger(EngineAggregatorModule.class.getName());
+	private static Map<String, EngineSubscriberResponseHandler> registry = new HashMap<String, EngineSubscriberResponseHandler>();
 
+	@Override
+	public void execute(JobExecutionContext context) throws JobExecutionException {
+		ApplicationConfigProvider.performSystemCheck();
+		Neo4jDBHandler graphDBHandler = new Neo4jDBHandler();
+		WebHookConfigDAL webhookConfigDal = new WebHookConfigDAL();
+		List<WebHookConfig> allWebhookConfigurations = webhookConfigDal.getAllWebHookConfigurations();
+		for (WebHookConfig webhookConfig : allWebhookConfigurations) {
+			String webhookname = webhookConfig.getWebHookName().toUpperCase();
+			String toolName = webhookConfig.getToolName().toUpperCase();
+			Boolean subscribeStatus = webhookConfig.getSubscribeStatus();
 
-    @Override
-    public void execute(JobExecutionContext context) throws JobExecutionException {
-        ApplicationConfigProvider.performSystemCheck();
-        Neo4jDBHandler graphDBHandler = new Neo4jDBHandler();
-        WebHookConfigDAL webhookConfigDal = new WebHookConfigDAL();
-        List < WebHookConfig > allWebhookConfigurations = webhookConfigDal.getAllWebHookConfigurations();
-        //boolean enableOnlineDatatagging = ApplicationConfigProvider.getInstance().isEnableOnlineDatatagging();
-        Map < String, List < WebhookMappingData >> webhookMappinMap = new HashMap < String, List < WebhookMappingData >> (0);
-        //if (enableOnlineDatatagging) {
-        //webhookMappinMap = getMetaData(graphDBHandler);
-        //	}
-        for (WebHookConfig webhookConfig: allWebhookConfigurations) {
-            String webhookname = webhookConfig.getWebHookName().toUpperCase();
-            String toolName = webhookConfig.getToolName().toUpperCase();
-            Boolean subscribeStatus = webhookConfig.getSubscribeStatus();
+			log.debug("Webhook Detail {}  subscribed Status {}", webhookname, subscribeStatus);
 
+			if (subscribeStatus == true) {
+				registerAggragators(webhookConfig, graphDBHandler, toolName);
+			}
+		}
+	}
 
-           // log.debug(toolName);
-            List < WebhookMappingData > webhookMappingList = webhookMappinMap.get(webhookname);
-            if (webhookMappingList == null) {
-                webhookMappingList = new ArrayList < WebhookMappingData > (0);
-            }
-            if (subscribeStatus == true) {
-                registerAggragators(webhookConfig, graphDBHandler, toolName, webhookMappingList);
-            }
-        }
-    }
-    private void registerAggragators(WebHookConfig webhookConfig, Neo4jDBHandler graphDBHandler, String toolName,
-        List < WebhookMappingData > webhookMappingList) {
-        try {
+	private void registerAggragators(WebHookConfig webhookConfig, Neo4jDBHandler graphDBHandler, String toolName) {
+		String dataRoutingKey = webhookConfig.getMQChannel();
+		String labelName = webhookConfig.getLabelName();
+		String webhookName = webhookConfig.getWebHookName();
+		String responseTemplate = webhookConfig.getResponseTemplate();
+		try {
+			if (dataRoutingKey != null && !registry.containsKey(dataRoutingKey)) {
+				try {
+					registry.put(dataRoutingKey, new WebHookDataSubscriber(dataRoutingKey, responseTemplate, toolName,
+							labelName, webhookName));
+					log.debug("Webhook {} subscribed successfully ", webhookName);
+					WebhookEngineStatusLogger.getInstance().createEngineStatusNode(
+							"Webhook " + webhookName + " subscribed successfully ", PlatformServiceConstants.SUCCESS);
+				} catch (Exception e) {
+					WebhookEngineStatusLogger.getInstance().createEngineStatusNode(
+							"Unable to subscribed Webhook " + webhookName + " Error Detail :" + e.getMessage(),
+							PlatformServiceConstants.FAILURE);
+					log.error("Unable to add subscriber for routing key: " + e);
 
-            String dataRoutingKey = webhookConfig.getMQChannel();
-            String labelName = webhookConfig.getLabelName();
-            String webhookName = webhookConfig.getWebHookName();
-            String responseTemplate = webhookConfig.getResponseTemplate();
+				}
+			}
+		} catch (Exception e) {
+			WebhookEngineStatusLogger.getInstance().createEngineStatusNode(
+					"Unable to subscribed Webhook " + webhookName + " Error Detail :" + e.getMessage(),
+					PlatformServiceConstants.FAILURE);
+			log.error("Unable to add subscriber for routing key: " + e);
 
-            if (dataRoutingKey != null && !registry.containsKey(dataRoutingKey)) {
-                try {
-                    registry.put(dataRoutingKey, new WebHookDataSubscriber(dataRoutingKey, responseTemplate, toolName,labelName,webhookName));
-                } catch (Exception e) {
-                    log.error("Unable to add subscriber for routing key: " + e);
+		}
 
-                }
-
-            }
-        } catch (Exception e) {
-            log.error("Unable to add subscriber for routing key: " + e);
-
-        }
-
-
-
-    }
+	}
 }
