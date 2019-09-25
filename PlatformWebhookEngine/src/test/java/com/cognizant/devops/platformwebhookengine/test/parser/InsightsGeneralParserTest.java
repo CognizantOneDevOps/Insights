674c54627788ece428105d5d682f1f992e88ae3e
Surbhi Gupta <surbhi.gupta@cognizant.com>
2019-09-25 17:22:03 +0530

    168508227 :- Addition of a test case and code formatting.

--- PlatformWebhookEngine/src/test/java/com/cognizant/devops/platformwebhookengine/test/parser/InsightsGeneralParserTest.java
+++ PlatformWebhookEngine/src/test/java/com/cognizant/devops/platformwebhookengine/test/parser/InsightsGeneralParserTest.java
@@ -25,7 +25,6 @@ import com.cognizant.devops.platformcommons.exception.InsightsCustomException;
 import com.cognizant.devops.platformwebhookengine.modules.aggregator.EngineAggregatorModule;
 import com.cognizant.devops.platformwebhookengine.parser.InsightsGeneralParser;
 import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
 
 @Test
 @ContextConfiguration(locations = { "classpath:spring-test-config.xml" })
@@ -39,32 +38,19 @@ public class InsightsGeneralParserTest extends InsightsParserTestData {
 
 		List<JsonObject> response = generalparser.parseToolData(testdata.responseTemplate, toolData, testdata.toolName,
 				testdata.labelName, testdata.webhookName);
-		JsonParser parser = new JsonParser();
-		/*
-		 * List<JsonObject> retrunJsonList = new ArrayList<JsonObject>(0); Gson
-		 * prettyGson = new GsonBuilder().setPrettyPrinting().create(); String
-		 * prettyJson = prettyGson.toJson(expectedOutput); JsonElement element =
-		 * parser.parse(prettyJson); retrunJsonList.add(element.getAsJsonObject());
-		 */
-		// Assert.assertSame(response,testdata.expectedOutput);
-		// List<JsonObject> participantJsonList =
-		// testdata.expectedOutput.readValue(jsonString, new
-		// TypeReference<List<JsonObject>>(){})
+		List<JsonObject> nullresponse = generalparser.parseToolData(testdata.fieldNotFoundinToolData, testdata.toolData,
+				testdata.toolName, testdata.labelName, testdata.webhookName);
+
 		String commitId = response.get(0).get("commitId").toString();
 		String authorName = response.get(0).get("authorName").toString();
 		String message = response.get(0).get("message").toString();
-
 		Assert.assertEquals(commitId, testdata.commitId);
 		Assert.assertEquals(authorName, testdata.authorName);
 		Assert.assertEquals(message, testdata.message);
 
-		/*
-		 * StringBuilder builder = new StringBuilder(response.toString());
-		 * builder.deleteCharAt(response.toString().length() - 1);
-		 * builder.deleteCharAt(0);
-		 * 
-		 * Assert.assertEquals(builder,testdata.expectedOutput);
-		 */
+		String responseTest = nullresponse.get(0).toString();
+		responseTest = responseTest.substring(1, responseTest.length() - 1); // remove curly brackets
+		Assert.assertEquals(responseTest, "");
 
 	}
 
