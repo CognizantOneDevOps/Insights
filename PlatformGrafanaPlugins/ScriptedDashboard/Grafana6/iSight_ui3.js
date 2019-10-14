1f21d38dbb03f22ef72f5b5ea6fc9756a57a846e
gauravdeshmukh612 <gaurav.deshmukh2@cognizant.com>
2019-10-14 16:38:55 +0530

    168626609 Grafana issue for isight

--- PlatformGrafanaPlugins/ScriptedDashboard/Grafana6/iSight_ui3.js
+++ PlatformGrafanaPlugins/ScriptedDashboard/Grafana6/iSight_ui3.js
@@ -64,10 +64,13 @@ var addDashboardIframeToDom = function (url) {
 		id: 'iSightIframe',
 		frameborder: 0,
 		width: '100%',
-		height:'100%' ,
+		height:'99%' ,
+		marginwidth:0 ,
+		marginheight:0 
 		//documentHeight
 		//style : 'overflow-y: hidden; width:'+width+'px; height:'+documentHeight+'px;'
-		scrolling: 'no'
+		//,scrolling: 'yes'
+		
 	}).appendTo('body');
 };
 
@@ -160,7 +163,7 @@ var addStyleTag = function () {
 		var style = "<style type=\"text/css\">" +
 			".sidemenu {display : none !important;}\n" +
 			".main-view{position:absolute;width:100%;-webkit-box-flex:1;-ms-flex-positive:1;flex-grow:1;height: calc(100% - 35px);)}\n" +
-			".scroll-canvas{position:fixed;width:100%;overflow:auto;-webkit-overflow-scrolling:touch;}\n" +
+			//".scroll-canvas{position:fixed;width:100%;overflow:auto;-webkit-overflow-scrolling:touch;}\n" +
 			".navbar-brand-btn {display : none !important;}\n" +
 			".page-header-canvas {display : none !important;}\n" +
 			".search-item-dash-home {display : none !important;}" +
