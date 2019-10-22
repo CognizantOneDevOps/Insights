47991194c4030ad8d977a73fce3eeda43ce1a90d
Surbhi Gupta <46189557+Surbhi546@users.noreply.github.com>
2019-10-22 13:53:54 +0530

    Update WebhookServiceTestData.java

--- PlatformService/src/test/java/com/cognizant/devops/platformservice/test/webhook/WebhookServiceTestData.java
+++ PlatformService/src/test/java/com/cognizant/devops/platformservice/test/webhook/WebhookServiceTestData.java
@@ -18,11 +18,11 @@ package com.cognizant.devops.platformservice.test.webhook;
 public class WebhookServiceTestData {
 
 	
-	String webhookname = "testNG_webhook_test2";
+	String webhookname = "testNG_webhook_test_new";
 	String toolName = "GIT";
 	String labelDisplay = "SCM:GIT:DATA";
 	String dataformat = "json";
-	String mqchannel = "IPW_testNG_webhook_test2";
+	String mqchannel = "IPW_testNG_webhook_test_new";
 	Boolean subscribestatus = true;
 	String responseTemplate = "head_commit.timestamp=commitTime,head_commit.author.name=authorName";
 }
