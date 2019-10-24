e1a08a4ccb2064a857cc7ece2c841d15c20cbea7
poojagupta285 <pooja.gupta9@cognizant.com>
2019-10-24 12:21:57 +0000

    #169112750 "Push Model Refactoring"

--- PlatformEnterpriseEngine/src/main/java/com/cognizant/devops/platformenterpriseengine/app/Application.java
+++ PlatformEnterpriseEngine/src/main/java/com/cognizant/devops/platformenterpriseengine/app/Application.java
@@ -21,7 +21,7 @@ import com.cognizant.devops.platformcommons.config.ApplicationConfigCache;
 import com.cognizant.devops.platformcommons.config.ApplicationConfigProvider;
 import com.cognizant.devops.platformcommons.constants.PlatformServiceConstants;
 import com.cognizant.devops.platformenterpriseengine.message.core.EnterpriseEngineStatusLogger;
-import com.cognizant.devops.platformenterpriseengine.platformwebhookengine.modules.aggregator.EngineAggregatorModule;
+
 
 import java.util.Timer;
 import java.util.TimerTask;
@@ -29,60 +29,64 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 /**
- * Engine execution will start from Application.
- * 1. Load the iSight config
- * 2. Initialize BlockChain and JiraProcessing Module.
+ * Engine execution will start from Application. 1. Load the iSight config 2.
+ * Initialize BlockChain and JiraProcessing Module.
  */
 public class Application {
-    private static Logger log = LogManager.getLogger(Application.class.getName());
+	private static Logger log = LogManager.getLogger(Application.class.getName());
 
-    private static int blockchainEngineInterval = 250;
-    private static int jiraEngineInterval = 250;
+	private static int blockchainEngineInterval = 250;
+	private static int jiraEngineInterval = 250;
 	private static int defaultIntervalInSec = 600;
 
-    private Application() {
+	private Application() {
 
-    }
+	}
 
-    public static void main(String[] args) {
-        if (args.length > 0) {
-            blockchainEngineInterval = Integer.valueOf(args[0]);
-        }
-        if (args.length >= 2) {
-            jiraEngineInterval = Integer.valueOf(args[1]);
-        }
-        try {
-            // Load insight configuration
-            ApplicationConfigCache.loadConfigCache();
+	public static void main(String[] args) {
+		if (args.length > 0) {
+			blockchainEngineInterval = Integer.valueOf(args[0]);
+		}
+		if (args.length >= 2) {
+			jiraEngineInterval = Integer.valueOf(args[1]);
+		}
+		try {
+			// Load insight configuration
+			ApplicationConfigCache.loadConfigCache();
 
-            ApplicationConfigProvider.performSystemCheck();
+			ApplicationConfigProvider.performSystemCheck();
 
-            // Schedule the BlockChainExecuter job
+			// Schedule the BlockChainExecuter job
 			Timer timerBlockChainProcessing = new Timer("BlockChainProcessingExecutor");
 			TimerTask blockChainProcessingTrigger = new PlatformAuditProcessingExecutor();
 			timerBlockChainProcessing.schedule(blockChainProcessingTrigger, 0, blockchainEngineInterval * 1000);
 
-			//Schedule the jira executor job
+			// Schedule the jira executor job
 			Timer timerJiraProcessing = new Timer("JiraProcessingExecutor");
 			TimerTask jiraProcessingTrigger = new JiraProcessingExecutor();
 			timerJiraProcessing.schedule(jiraProcessingTrigger, 0, jiraEngineInterval * 1000);
 
-            log.info("PlatformAudit Engine Service Started ", PlatformServiceConstants.SUCCESS);
-            
-            //Schedule WebhookEngine job
-            Timer timerWebhookEngineJobExecutorModule = new Timer("WebhookEngineJobExecutorModule");
-			TimerTask webhookAggregatorTrigger = new EngineAggregatorModule();
-			timerWebhookEngineJobExecutorModule.schedule(webhookAggregatorTrigger, 0, defaultIntervalInSec * 1000);
+			log.info("PlatformAudit Engine Service Started ", PlatformServiceConstants.SUCCESS);
 
-			EnterpriseEngineStatusLogger.getInstance().createEngineStatusNode("Platform Enterprise Engine Service Started ",PlatformServiceConstants.SUCCESS);
-            
+			// Schedule WebhookEngine job
+			Timer timerWebhookEngineJobExecutorModule = new Timer("WebhookEngineJobExecutorModule");
+			Class webhookAggregatorTrigger = Class
+					.forName("com.cognizant.devops.platformenterpriseengine.platformwebhookengine.modules.aggregator.EngineAggregatorModule");
+			TimerTask webHookTimer = (TimerTask) webhookAggregatorTrigger.newInstance();
+
+			timerWebhookEngineJobExecutorModule.schedule(webHookTimer, 0, defaultIntervalInSec * 1000);
+
+			EnterpriseEngineStatusLogger.getInstance().createEngineStatusNode(
+					"Platform Enterprise Engine Service Started ", PlatformServiceConstants.SUCCESS);
+
 		} catch (Exception e) {
 			EnterpriseEngineStatusLogger.getInstance().createEngineStatusNode(
-					"Platform Enterprise Engine Service not running " + e.getMessage(), PlatformServiceConstants.FAILURE);
+					"Platform Enterprise Engine Service not running " + e.getMessage(),
+					PlatformServiceConstants.FAILURE);
 			log.error(e);
-            log.info("PlatformAudit Engine Service not running " + e.getMessage(), PlatformServiceConstants.FAILURE);
-            log.error(e);
-        }
-    }
+			log.info("PlatformAudit Engine Service not running " + e.getMessage(), PlatformServiceConstants.FAILURE);
+			log.error(e);
+		}
+	}
 
 }
