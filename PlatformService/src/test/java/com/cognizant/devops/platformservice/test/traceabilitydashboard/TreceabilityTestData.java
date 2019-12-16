031906f49d77bc0f4fc7a1ca43bb0585ca46b523
SanketCTSI <54664618+SanketCTSI@users.noreply.github.com>
2019-12-16 11:10:41 +0530

    #170254861 : Test case support for Traceability Dashboard. (#122)

---
+++ PlatformService/src/test/java/com/cognizant/devops/platformservice/test/traceabilitydashboard/TreceabilityTestData.java
@@ -0,0 +1,26 @@
+/*******************************************************************************
+ * Copyright 2017 Cognizant Technology Solutions
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License.  You may obtain a copy
+ * of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ ******************************************************************************/
+package com.cognizant.devops.platformservice.test.traceabilitydashboard;
+
+public class TreceabilityTestData {
+	
+	public static String toolName="JIRA";
+	public static String fieldName="jiraKey";
+	public static String fieldVal ="LS-7145856944";
+	public static String incorrectFieldVal ="incorrect";
+	public static String cacheKey=toolName+fieldName+fieldVal;
+
+}
