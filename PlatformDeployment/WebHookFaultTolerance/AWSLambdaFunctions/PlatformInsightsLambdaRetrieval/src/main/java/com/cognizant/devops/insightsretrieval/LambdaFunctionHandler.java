b0469daa2dc9aac35bd2afe8c4fc995f968c5531
siddarthap93 <42437031+siddarthap93@users.noreply.github.com>
2020-03-04 12:47:50 +0000

    170814611 Aws cloudformation (#182)
    * 170814611 updated lambda function
    
    * 170814611 updated lambda function
    
    * 170814611 updated lambda function
    
    * 170814611 updated lambda function
    
    * 170814611 updated lambda function
    
    * 170814611 updated CloudFormation template
    
    * 170814611 updated pom.xml
    
    * 170814611 updated pom.xml

--- PlatformDeployment/WebHookFaultTolerance/AWSLambdaFunctions/PlatformInsightsLambdaRetrieval/src/main/java/com/cognizant/devops/insightsretrieval/LambdaFunctionHandler.java
+++ PlatformDeployment/WebHookFaultTolerance/AWSLambdaFunctions/PlatformInsightsLambdaRetrieval/src/main/java/com/cognizant/devops/insightsretrieval/LambdaFunctionHandler.java
@@ -28,43 +28,45 @@ import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.google.gson.JsonStreamParser;
 
+import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
 import software.amazon.awssdk.services.s3.model.GetObjectRequest;
 import software.amazon.awssdk.services.s3.model.ListObjectsRequest;
 import software.amazon.awssdk.services.s3.model.ListObjectsResponse;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 import software.amazon.awssdk.services.s3.model.S3Object;
 
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.Reader;
 import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.List;
 import java.util.ListIterator;
 
 import org.apache.commons.codec.binary.Base64;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class LambdaFunctionHandler implements RequestHandler<Object, String> {
 
 
 	private static final String S3_BUCKET_NAME =  System.getenv("s3BucketName");
 	private static final String REGION = System.getenv("s3BucketRegion");
-	private S3Client s3 = S3Client.builder().region(Region.of(REGION)).build();
+	private static final S3Client s3 = S3Client.builder().region(Region.of(REGION)).build();
 	private static final String FUNCTION_FAILED = "function-failed";
 	private static final String CONNECTION_FAILED = "connection-failed";
-	private static final int LAMBDA_CONNECTION_TIMEOUT = 5000;
+	private static final int CONNECTION_TIMEOUT = Integer.valueOf(System.getenv("connectionTimeout"));
 	private static final Integer MAX_FILE_READ_COUNT = 2;
-	private static Logger logger = LoggerFactory.getLogger(LambdaFunctionHandler.class);
+	private static Logger log = LogManager.getLogger(LambdaFunctionHandler.class);
 
+	private StringBuffer failedData;
 	private int responseCode;
-	private boolean deleteObject;
 
 	/**
 	 * Retrieval Lambda Handler to retrieve data form S3 Bucket and send the data
@@ -92,18 +94,21 @@ public class LambdaFunctionHandler implements RequestHandler<Object, String> {
 			ListObjectsResponse response = s3.listObjects(listObjects);
 			List<S3Object> objects = response.contents();
 
-			responseCode = 200;
 			for (ListIterator<S3Object> iterVals = objects.listIterator(); iterVals.hasNext(); ) {
 				S3Object s3Keys = (S3Object) iterVals.next();
 				if (s3Keys != null) {
-					getData(s3Keys, prefix);
-					if (responseCode == 200 && deleteObject == true) {
-						deleteData(s3Keys);
+					processData(s3Keys, prefix);
+					if (failedData.length() == 0) {
+						deleteObject(s3Keys);
+					}
+					else {
+						deleteObject(s3Keys);
+						updateObject(s3Keys, failedData);
 					}
 				}
 			}
 		} catch (Exception e) {
-			logger.error("Could not able to list object form S3 Bucket " + e.toString());
+			log.error("Could not able to list object form S3 Bucket " + e.toString());
 		}
 	}
 
@@ -113,74 +118,83 @@ public class LambdaFunctionHandler implements RequestHandler<Object, String> {
 	 * @param prefix (S3 key or Folder Name)
 	 */
 
-	public void getData(S3Object parseKey, String prefix){
+	public void processData(S3Object parseKey, String prefix){
 		JsonObject finalJson;
 		GetObjectRequest getObjectRequest = GetObjectRequest.builder().bucket(S3_BUCKET_NAME).key(parseKey.key()).build();
 		InputStream s3Object = s3.getObject(getObjectRequest);
 		try {
 			Reader reader = new InputStreamReader(s3Object, "UTF-8");
 			JsonStreamParser parser = new JsonStreamParser(reader);
+			failedData = new StringBuffer();
 			Gson gson = new GsonBuilder().create();
+			responseCode = 200;
 			while (parser.hasNext()) {
-				JsonElement element = parser.next();
-				String data = gson.toJson(element);
-				JsonElement parsedElement = new JsonParser().parse(data);
-				JsonObject encodedJson = parsedElement.getAsJsonObject();
-				if (prefix == FUNCTION_FAILED) {
-					String jsonString= encodedJson.get("rawData").toString();
-					String decodedString = new String(Base64.decodeBase64(jsonString.getBytes()));
-					JsonElement rawData = new JsonParser().parse(decodedString);
-					finalJson = rawData.getAsJsonObject();
-				}else {
-					finalJson = encodedJson;
-				}
-				String body = finalJson.get("body").toString();
-				String tool = finalJson.get("tool").getAsString();
-				String url = System.getenv(tool);
-				if (responseCode == 200 && url != null) {
-					sendData(body, url);
-					deleteObject = true;
-				}else {
-					deleteObject = false;
-					logger.error("Could not connect to WebHook endpoint tool : " + tool + "url : " + url );
-					break;
-				}
+				try {
+					JsonElement element = parser.next();
+					String data = gson.toJson(element);
+					JsonElement parsedElement = new JsonParser().parse(data);
+					JsonObject encodedJson = parsedElement.getAsJsonObject();
+					if (prefix == FUNCTION_FAILED) {
+						String jsonString= encodedJson.get("rawData").toString();
+						String decodedString = new String(Base64.decodeBase64(jsonString.getBytes()));
+						JsonElement rawData = new JsonParser().parse(decodedString);
+						finalJson = rawData.getAsJsonObject();
+					}else {
+						finalJson = encodedJson;
+					}
+					String body = finalJson.get("body").toString();
+					String tool = finalJson.get("tool").getAsString();
+					String url = System.getenv(tool);
+					sendData(element, body, url);
+				} catch (Exception e) {
+					log.error("Could able to process the data" + e.toString());
+				} 
 			}
-
 		} catch (UnsupportedEncodingException e) {
-			logger.error("Could able to parse the data UnsupportedEncodingException" + e.toString());
+			log.error("Could able to parse the data UnsupportedEncodingException" + e.toString());
 		}
-	}
+
+	} 
 
 	/**
 	 * Send the events to Webhook endpoint
-	 * @param inputData
+	 * @param data
+	 * @param body
 	 * @param url
 	 * @return responseCode
 	 */
 
-	public int sendData(String inputData, String url){
+	public int sendData(JsonElement data, String body, String url){
 
 		try {
-			URL insightsUrl = new URL(url);
-			HttpURLConnection con;
-			con = (HttpURLConnection)insightsUrl.openConnection();
-			con.setRequestMethod("POST");
-			con.setRequestProperty("Content-Type", "application/json; utf-8");
-			con.setRequestProperty("Accept", "application/json");
-			con.setDoOutput(true);
-			con.setConnectTimeout(LAMBDA_CONNECTION_TIMEOUT);
-			OutputStream os = con.getOutputStream();
-			os.write(inputData.getBytes());
-			os.flush();
-			os.close();
-			responseCode = con.getResponseCode();
-		} catch (IOException e) {
-			logger.error("Error ouccured while send message to WebHook endpoint" + e.toString());
-			deleteObject = false;
+			if (responseCode == 200 && url != null) {
+				responseCode = 0;
+				URL insightsUrl = new URL(url);
+				HttpURLConnection con = (HttpURLConnection)insightsUrl.openConnection();
+				con.setRequestMethod("POST");
+				con.setRequestProperty("Content-Type", "application/json; utf-8");
+				con.setRequestProperty("Accept", "application/json");
+				con.setDoOutput(true);
+				con.setConnectTimeout(CONNECTION_TIMEOUT);
+				OutputStream os = con.getOutputStream();
+				os.write(body.getBytes());
+				os.flush();
+				os.close();
+				responseCode = con.getResponseCode();
+				if  (responseCode != 200) {
+					log.error("Could not connect URL " + url + " return response code " + responseCode);
+				}
+			}
+		} catch (MalformedURLException e) {
+			log.error("Invalid URL fot the tool " + e.toString());
+		} catch (Exception e) {
+			log.error("Error connecting to URL " + url + " " + e.toString());
+		} finally {
+			if (responseCode != 200) {
+				failedData.append(data);
+			}
 		}
 		return responseCode;
-
 	}
 
 	/**
@@ -188,13 +202,28 @@ public class LambdaFunctionHandler implements RequestHandler<Object, String> {
 	 * @param deleteKey
 	 */
 
-	public void deleteData(S3Object deleteKey) {
+	public void deleteObject(S3Object deleteKey) {
 
 		try {
 			DeleteObjectRequest deleteObjectRequest = DeleteObjectRequest.builder().bucket(S3_BUCKET_NAME).key(deleteKey.key()).build();
 			s3.deleteObject(deleteObjectRequest);
 		} catch (Exception e) {
-			logger.error("Error while deleting the object form S3" + e.toString());
+			log.error("Error while deleting the object form S3" + e.toString());
+		}
+	}	
+
+	/**
+	 * Update the Failure events to S3Bucket
+	 * @param updateKey
+	 * @param updateDsta
+	 */
+
+	public void updateObject(S3Object updateKey, StringBuffer updateDsta) {
+		try {
+			PutObjectRequest putObjectRequest = PutObjectRequest.builder().bucket(S3_BUCKET_NAME).key(updateKey.key()).build();
+			s3.putObject(putObjectRequest, RequestBody.fromString(updateDsta.toString()));
+		} catch (Exception e) {
+			log.error("Error while updating the object to S3" + e.toString());
 		}
 	}
 }
