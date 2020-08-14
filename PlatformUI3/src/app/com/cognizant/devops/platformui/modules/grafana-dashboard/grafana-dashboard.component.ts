f815727ea77f58e16ad4a28651dfb3c2e9e3c3bd
Sapna446 <58971333+Sapna446@users.noreply.github.com>
2020-08-14 10:47:47 +0530

    ONEDEVOPS-914: Logged in User Details inside Landing Page needs to be configurable (#249)
    * ONEDEVOPS-914: toolbar display changes
    
    * #ONEDEVOPS-914: toolbar display changes

--- PlatformUI3/src/app/com/cognizant/devops/platformui/modules/grafana-dashboard/grafana-dashboard.component.ts
+++ PlatformUI3/src/app/com/cognizant/devops/platformui/modules/grafana-dashboard/grafana-dashboard.component.ts
@@ -42,14 +42,19 @@ export class GrafanaDashboardComponent implements OnInit {
     defaultOrg: number;
     selectedApp: string;
     framesize: any;
+    offset: number;
     selectedDashboardUrl: string = '';
     selectedDashboard: GrafanaDashboardMode;
     dashboards = [];
     constructor(private route: ActivatedRoute, private router: Router,
         private sanitizer: DomSanitizer, private grafanadashboardservice: GrafanaDashboardService, private cookieService: CookieService) {
         var self = this;
-        var offset = 55; // 5px is app-grafana-dashboard height true 5
-        this.framesize = window.frames.innerHeight - offset
+        if(InsightsInitService.enableInsightsToolbar) {
+            this.offset = 55; // 5px is app-grafana-dashboard height true 5
+        } else {
+            this.offset = 0;
+        }
+        this.framesize = window.frames.innerHeight - this.offset
         console.log(" self.framesize inner height " + self.framesize);
         var receiveMessage = function (evt) {
             var height = parseInt(evt.data);
