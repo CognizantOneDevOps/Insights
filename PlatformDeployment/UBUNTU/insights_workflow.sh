f38ab558d972cf1ae50136cd54fc5c3ea8c5e1b0
Surbhi Gupta <46189557+Surbhi546@users.noreply.github.com>
2020-09-23 14:40:15 +0530

    Scripts for workflow  (#262)
    * #ONEDEVOPS-950 :-Scripts for workflow
    
    * #ONEDEVOPS-950 :- Fixing the commands in the scripts
    
    * #ONEDEVOPS-1006 -Spell check
    
    * #ONEDEVOPS-950 modifications in workflow scripts

---
+++ PlatformDeployment/UBUNTU/insights_workflow.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#-------------------------------------------------------------------------------
+# Copyright 2017 Cognizant Technology Solutions
+#   
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not
+# use this file except in compliance with the License.  You may obtain a copy
+# of the License at
+# 
+#   http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+# License for the specific language governing permissions and limitations under
+# the License.
+#-------------------------------------------------------------------------------
+# get insights Workflow jar
+echo "#################### Getting Insights Workflow Jar ####################"
+sudo mkdir /opt/insightsworkflow
+cd /opt/insightsworkflow
+export INSIGHTS_WORKFLOW=`pwd`
+sudo echo INSIGHTS_WORKFLOW=`pwd` | sudo tee -a /etc/environment
+sudo echo "export" INSIGHTS_WORKFLOW=`pwd` | sudo tee -a /etc/profile
+. /etc/environment
+. /etc/profile
+cd /opt/insightsworkflow
+sudo wget https://platform.cogdevops.com/insights_install/release/latest/PlatformWorkflow.jar -O PlatformWorkflow.jar
+sleep 2
+INSIGHTS_HOME=/usr/INSIGHTS_HOME
+sudo nohup java -cp PlatformWorkflow.jar:$INSIGHTS_HOME/workflowjar/* com.cognizant.devops.platformworkflow.workflowtask.app.PlatformWorkflowApplication &
