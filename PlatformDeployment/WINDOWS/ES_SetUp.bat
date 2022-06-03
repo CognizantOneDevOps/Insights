e86a373da56a2773e2047d15cc5eb15c4556b640
p-chavan <root@ip-178-16-1-159.ec2.internal>
2022-06-03 06:34:11 +0000

    #ONEDEVOPS-1734 adding license text

--- PlatformDeployment/WINDOWS/ES_SetUp.bat
+++ PlatformDeployment/WINDOWS/ES_SetUp.bat
@@ -12,6 +12,7 @@ distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
 WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
 License for the specific language governing permissions and limitations under
 the License.
+:comment
 echo "Setting up Elastic Search as windows service"
 call %~dp0elasticsearch-5.6.4\bin\elasticsearch-service install
 Timeout 2
--- PlatformDeployment/WINDOWS/Neo4j_StartUp.bat
+++ PlatformDeployment/WINDOWS/Neo4j_StartUp.bat
@@ -15,4 +15,3 @@ the License.
 :comment
 echo "Start Neo4j"
 net start neo4j
-Timeout 2
